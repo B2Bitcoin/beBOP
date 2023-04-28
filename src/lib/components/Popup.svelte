f55d1e22e11555bf810f08e0f868fbffe50b13ab
coyotte508 <coyotte508@gmail.com>
2023-04-28 22:14:08 +0200

    ✨ Create popup component

---
+++ src/lib/components/Popup.svelte
@@ -0,0 +1,7 @@
+<div class="absolute z-10 -bottom-7 -right-2 w-[562px] max-w-[95vw] translate-y-full">
+	<div
+		class="bg-gray-40 rounded drop-shadow-[0_4px_10px_rgba(0,0,0,0.13)] after:absolute after:z-20 after:top-0 after:right-[30px] after:w-0 after:h-0 after:-bottom-1 after:-translate-y-full after:border-l-[10px] after:border-l-transparent after:border-r-[10px] after:border-r-transparent after:border-b-[18px] after:border-b-gray-40"
+	>
+		<slot />
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,7 @@
 	import ProductAddedToCart from '$lib/components/ProductAddedToCart.svelte';
 	import { productAddedToCart } from '$lib/stores/productAddedToCart';
 	import { sum } from '$lib/utils/sum';
+	import Popup from '$lib/components/Popup.svelte';
 
 	export let data;
 
@@ -90,14 +91,14 @@
 					{cartItems}
 				</a>
 				{#if $productAddedToCart}
-					<div class="absolute z-10 -bottom-7 -right-2 w-[562px] max-w-[95vw] translate-y-full">
+					<Popup>
 						<ProductAddedToCart
-							class="bg-gray-40 drop-shadow-[0_4px_10px_rgba(0,0,0,0.13)] after:absolute after:z-20 after:top-0 after:right-[30px] after:w-0 after:h-0 after:-bottom-1 after:-translate-y-full after:border-l-[10px] after:border-l-transparent after:border-r-[10px] after:border-r-transparent after:border-b-[18px] after:border-b-gray-40"
+							class=""
 							on:dismiss={() => ($productAddedToCart = null)}
 							product={$productAddedToCart.product}
 							picture={$productAddedToCart.picture}
 						/>
-					</div>
+					</Popup>
 				{/if}
 			</div>
 		</div>
