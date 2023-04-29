aa0603fd490e605a27331199d8b78dec34bff59c
coyotte508 <coyotte508@gmail.com>
2023-04-29 19:11:02 +0200

    ✨ "Buy now" button works

--- src/routes/product/[id]/+page.server.ts
+++ src/routes/product/[id]/+page.server.ts
@@ -1,6 +1,6 @@
 import { collections } from '$lib/server/database';
-import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { error, redirect } from '@sveltejs/kit';
+import type { PageServerLoad, RequestEvent } from './$types';
 import { productToFrontend, type Product } from '$lib/types/Product';
 import { z } from 'zod';
 import { ObjectId } from 'mongodb';
@@ -31,63 +31,67 @@ export const load: PageServerLoad = async ({ params }) => {
 	};
 };
 
+async function addToCart({ params, request, locals }: RequestEvent) {
+	const productExists = !!(await collections.products.countDocuments({ _id: params.id }));
+
+	if (!productExists) {
+		throw error(404, 'Product not found');
+	}
+
+	const formData = await request.formData();
+	const { quantity } = z
+		.object({
+			quantity: z.number({ coerce: true }).int().min(1).max(MAX_PRODUCT_QUANTITY)
+		})
+		.parse({
+			quantity: formData.get('quantity')
+		});
+
+	let cart = await collections.carts.findOne({ sessionId: locals.sessionId });
+
+	if (!cart) {
+		cart = {
+			_id: new ObjectId(),
+			sessionId: locals.sessionId,
+			items: [],
+			createdAt: new Date(),
+			updatedAt: new Date()
+		};
+	}
+
+	const existingItem = cart.items.find((item) => item.productId === params.id);
+
+	if (existingItem) {
+		existingItem.quantity += quantity;
+	} else {
+		cart.items.push({
+			productId: params.id,
+			quantity
+		});
+	}
+
+	await collections.carts.updateOne(
+		{ _id: cart._id },
+		{
+			$set: {
+				items: cart.items,
+				updatedAt: new Date()
+			},
+			$setOnInsert: {
+				createdAt: new Date(),
+				sessionId: locals.sessionId
+			}
+		},
+		{ upsert: true }
+	);
+}
+
 export const actions = {
-	checkout: async () => {
-		// blabla
+	buy: async (params) => {
+		await addToCart(params);
+
+		throw redirect(303, '/cart');
 	},
 
-	addToCart: async ({ request, params, locals }) => {
-		const productExists = !!(await collections.products.countDocuments({ _id: params.id }));
-
-		if (!productExists) {
-			throw error(404, 'Resource not found');
-		}
-
-		const formData = await request.formData();
-		const { quantity } = z
-			.object({
-				quantity: z.number({ coerce: true }).int().min(1).max(MAX_PRODUCT_QUANTITY)
-			})
-			.parse({
-				quantity: formData.get('quantity')
-			});
-
-		let cart = await collections.carts.findOne({ sessionId: locals.sessionId });
-
-		if (!cart) {
-			cart = {
-				_id: new ObjectId(),
-				sessionId: locals.sessionId,
-				items: [],
-				createdAt: new Date(),
-				updatedAt: new Date()
-			};
-		}
-
-		const existingItem = cart.items.find((item) => item.productId === params.id);
-
-		if (existingItem) {
-			existingItem.quantity += quantity;
-		} else {
-			cart.items.push({
-				productId: params.id,
-				quantity
-			});
-		}
-
-		await collections.carts.updateOne(
-			{ _id: cart._id },
-			{
-				$set: {
-					items: cart.items,
-					updatedAt: new Date()
-				},
-				$setOnInsert: {
-					createdAt: new Date(),
-					sessionId: locals.sessionId
-				}
-			},
-			{ upsert: true }
-		);
-	}
+	addToCart
 };
