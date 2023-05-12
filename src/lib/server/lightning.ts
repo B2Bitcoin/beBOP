998f5131a1819d2675cbfb71b67a30d2f98f2a3e
coyotte508 <coyotte508@gmail.com>
2023-05-12 19:59:51 +0200

    🐛 Fix lnd calls on localhost

--- src/lib/server/lightning.ts
+++ src/lib/server/lightning.ts
@@ -1,4 +1,6 @@
 import { readFileSync } from 'node:fs';
+// We use undici instead of native fetch to be able to accept self-signed certificates
+import { Agent, fetch } from 'undici';
 import { LND_MACAROON_PATH, LND_MACAROON_VALUE, LND_REST_URL } from '$env/static/private';
 import { z } from 'zod';
 
@@ -10,12 +12,32 @@ const macaroon = LND_MACAROON_PATH
 	? readFileSync(LND_MACAROON_PATH).toString('hex')
 	: LND_MACAROON_VALUE;
 
-export async function lndListChannels() {
-	const response = await fetch(`${LND_REST_URL}/v1/channels`, {
+const url = new URL(LND_REST_URL);
+
+// For https://localhost or https://127.0.0.1, we need to disable TLS verification
+const dispatcher =
+	url.protocol === 'https:' &&
+	(url.hostname === 'localhost' || url.hostname === '127.0.0.1' || url.hostname === '[::1]')
+		? new Agent({
+				connect: {
+					rejectUnauthorized: false
+				}
+		  })
+		: undefined;
+
+function lndRpc(path: string, options: { method?: string; headers?: Record<string, string> } = {}) {
+	return fetch(`${LND_REST_URL}${path}`, {
 		headers: {
+			...options.headers,
 			...(macaroon && { 'Grpc-Metadata-macaroon': macaroon })
-		}
+		},
+		method: options.method || 'GET',
+		...(dispatcher && { dispatcher })
 	});
+}
+
+export async function lndListChannels() {
+	const response = await lndRpc('/v1/channels');
 
 	const json = await response.json();
 	return z
@@ -33,11 +55,7 @@ export async function lndListChannels() {
 }
 
 export async function lndWalletBalance() {
-	const response = await fetch(`${LND_REST_URL}/v1/balance/blockchain`, {
-		headers: {
-			...(macaroon && { 'Grpc-Metadata-macaroon': macaroon })
-		}
-	});
+	const response = await lndRpc('/v1/balance/blockchain');
 
 	const json = await response.json();
 	return z
@@ -48,22 +66,14 @@ export async function lndWalletBalance() {
 		.parse(json).total_balance;
 }
 export async function lndGetInfo() {
-	const response = await fetch(`${LND_REST_URL}/v1/getinfo`, {
-		headers: {
-			...(macaroon && { 'Grpc-Metadata-macaroon': macaroon })
-		}
-	});
+	const response = await lndRpc('/v1/getinfo');
 
 	const json = await response.json();
 	return z.object({ alias: z.string(), testnet: z.boolean() }).parse(json);
 }
 
 export async function lndChannelsBalance() {
-	const response = await fetch(`${LND_REST_URL}/v1/balance/channels`, {
-		headers: {
-			...(macaroon && { 'Grpc-Metadata-macaroon': macaroon })
-		}
-	});
+	const response = await lndRpc('/v1/balance/channels');
 
 	const json = await response.json();
 	return z.object({ balance: z.number({ coerce: true }).int() }).parse(json).balance;
