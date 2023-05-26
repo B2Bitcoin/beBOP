bc7804653f94ad354ccb201c59a4686f193828bf
Abdou Aziz Ba <baabdouaziz3@gmail.com>
2023-05-26 09:23:26 +0000

    fix product minimum price to 1 SAT

--- src/routes/admin/product/[id]/+page.svelte
+++ src/routes/admin/product/[id]/+page.svelte
@@ -10,6 +10,9 @@
 	let availableDateStr = availableDate?.toJSON().slice(0, 10);
 	let preorder = data.product.preorder;
 
+	let priceAmount: number;
+	let priceAmountElement: HTMLInputElement;
+
 	$: changedDate = availableDateStr !== availableDate?.toJSON().slice(0, 10);
 	$: enablePreorder = availableDateStr && availableDateStr > new Date().toJSON().slice(0, 10);
 
@@ -21,12 +24,23 @@
 		availableDateStr = undefined;
 		availableDate = undefined;
 	}
+
+	function checkForm(event: SubmitEvent) {
+		if (priceAmountElement.value && +priceAmountElement.value < 0.00000001) {
+			priceAmountElement.setCustomValidity('price must be greater than 1 SAT');
+			priceAmountElement.reportValidity();
+			event.preventDefault();
+			return;
+		} else {
+			priceAmountElement.setCustomValidity('');
+		}
+	}
 </script>
 
 <h1 class="text-3xl">Edit a product</h1>
 
 <div class="flex flex-col">
-	<form method="post" class="flex flex-col gap-4" action="?/update">
+	<form method="post" class="flex flex-col gap-4" action="?/update" on:submit={checkForm}>
 		<label>
 			Name
 			<input type="text" name="name" class="form-input block" value={data.product.name} />
@@ -39,6 +53,8 @@
 				class="form-input block"
 				step="any"
 				value={data.product.price.amount.toLocaleString('en', { maximumFractionDigits: 8 })}
+				bind:this={priceAmountElement}
+				on:change={() => priceAmountElement?.setCustomValidity('')}
 			/>
 		</label>
 
