ae937eb530a3a33f327873db14ede72ddfd34eab
coyotte508 <coyotte508@gmail.com>
2023-06-02 18:23:20 +0200

    fixup! Disallow some pages for SEO (#120)

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,9 +1,16 @@
 export function GET() {
-	return new Response(`User-agent: *
+	return new Response(
+		`User-agent: *
 Disallow: /order/
 Disallow: /subscription/
 Disallow: /admin/
 Disallow: /checkout
 Disallow: /cart
-`);
+`,
+		{
+			headers: {
+				'content-type': 'text/plain'
+			}
+		}
+	);
 }
