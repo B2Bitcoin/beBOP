b3c40fa2e8bc5884fcaf31aa7a155d20c613cb6e
Eliott C <coyotte508@gmail.com>
2023-06-08 14:47:47 +0200

    ✨ Able to include products in CMS pages (#139)

--- src/lib/server/picture.ts
+++ src/lib/server/picture.ts
@@ -173,3 +173,19 @@ export async function deletePicture(pictureId: Picture['_id']) {
 		.send(new DeleteObjectCommand({ Bucket: S3_BUCKET, Key: res.value.storage.original.key }))
 		.catch(console.error);
 }
+
+export function picturesForProducts(productIds: string[]): Promise<Picture[]> {
+	return collections.pictures
+		.aggregate<Picture>([
+			{ $match: { productId: { $in: productIds } } },
+			{ $sort: { createdAt: 1 } },
+			{
+				$group: {
+					_id: '$productId',
+					value: { $first: '$$ROOT' }
+				}
+			},
+			{ $replaceRoot: { newRoot: '$value' } }
+		])
+		.toArray();
+}
--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,5 +1,10 @@
 import { collections } from '$lib/server/database.js';
 import { error } from '@sveltejs/kit';
+import { marked } from 'marked';
+import type { Product } from '$lib/types/Product.js';
+import { picturesForProducts } from '$lib/server/picture.js';
+
+const PRODUCT_WIDGET_REGEX = /^\[Product=(?<slug>[a-z0-9-]+)\]$/i;
 
 export async function load({ params }) {
 	const cmsPage = await collections.cmsPages.findOne({
@@ -10,5 +15,43 @@ export async function load({ params }) {
 		throw error(404, 'CMS Page not found');
 	}
 
-	return { cmsPage };
+	const productSlugs = new Set<string>();
+
+	const tokens = marked.lexer(cmsPage.content).map((token) => {
+		if (token.type === 'paragraph') {
+			const match = token.raw.match(PRODUCT_WIDGET_REGEX);
+
+			if (match?.groups?.slug) {
+				const slug = match.groups.slug;
+
+				productSlugs.add(slug);
+
+				return {
+					type: 'productWidget',
+					raw: token.raw,
+					slug
+				};
+			}
+		}
+
+		return token;
+	});
+
+	const products = await collections.products
+		.find({
+			_id: { $in: [...productSlugs] }
+		})
+		.project<Pick<Product, '_id' | 'price' | 'description' | 'name'>>({
+			price: 1,
+			description: 1,
+			name: 1
+		})
+		.toArray();
+
+	return {
+		cmsPage,
+		tokens,
+		products,
+		pictures: picturesForProducts(products.map((product) => product._id))
+	};
 }
