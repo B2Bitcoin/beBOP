b36f1e0e6e9d08e9182835b482ca607090f76fe2
ithiame <133223073+ithiame@users.noreply.github.com>
2023-06-08 11:14:38 +0000

    As a bootik owner, I want to be able to generate a preview #102 (#132)

---
+++ src/lib/components/ProductWidget.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import type { Product } from '$lib/types/Product';
+	import type { Picture } from '$lib/types/Picture';
+	import PictureComponent from './Picture.svelte';
+	import PriceTag from './PriceTag.svelte';
+
+	export let picture: Picture | undefined;
+	export let product: Pick<Product, '_id' | 'name' | 'price' | 'description'>;
+	export let exchangeRate = 0;
+</script>
+
+<div class="flex flex-col text-center">
+	<a href="/product/{product._id}" class="flex flex-col items-center">
+		<PictureComponent {picture} class="object-contain max-h-[250px] max-w-full" />
+	</a>
+</div>
+
+<div class="flex flex-col m-6">
+	<div class="flex flex-row gap-2">
+		<a href="/product/{product._id}" class="flex flex-col items-center">
+			<h2 class="text-4xl">{product.name}</h2>
+		</a>
+
+		<div class="grow" />
+
+		<div class="flex flex-row items-end justify-center">
+			<PriceTag
+				amount={product.price.amount}
+				currency={product.price.currency}
+				class="text-3xl text-gray-800"
+			/>
+			&nbsp; ~ &nbsp;
+			<PriceTag
+				class="text-base text-gray-600"
+				amount={product.price.amount}
+				currency={product.price.currency}
+				{exchangeRate}
+				convertedTo="EUR"
+			/>
+		</div>
+	</div>
+	<a href="/product/{product._id}" class="flex flex-col items-center">
+		<p class="text-2xl mt-4 text-gray-800">
+			{product.description}
+		</p>
+	</a>
+	<div class="flex flex-row items-end justify-end">
+		<button value="Add to cart" formaction="?/addToCart" class="btn btn-gray"> Add to cart </button>
+	</div>
+</div>
---
+++ src/routes/product/[id]/widget/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ProductWidget from '$lib/components/ProductWidget.svelte';
+
+	export let data;
+</script>
+
+<main class="mx-auto max-w-7xl py-10 px-6">
+	<article class="w-full rounded-xl bg-white border-gray-300 border py-3 px-3 flex flex-col gap-2">
+		<ProductWidget product={data.product} picture={data.picture} exchangeRate={data.exchangeRate} />
+	</article>
+</main>
