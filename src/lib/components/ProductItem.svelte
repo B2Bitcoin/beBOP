847c470bf907c79bf1b762f2bff124b8c9ad60be
coyotte508 <coyotte508@gmail.com>
2023-06-08 22:09:02 +0200

    ✨ Add cancel button on order + pictures

--- src/lib/components/ProductItem.svelte
+++ src/lib/components/ProductItem.svelte
@@ -6,13 +6,16 @@
 	export let picture: Picture | undefined;
 	export let product: Pick<Product, '_id' | 'name'>;
 	export let isAdmin = false;
+	let className = '';
+	export { className as class };
 </script>
 
-<div class="flex flex-col text-center">
+<div class="flex flex-col text-center {className}">
 	<a href="{isAdmin ? '/admin' : ''}/product/{product._id}" class="flex flex-col items-center">
 		<PictureComponent {picture} class="block h-36" />
 		<span class="mt-2 line-clamp-3 text-ellipsis max-w-[192px] break-words hyphens-auto"
 			>{product.name}</span
 		>
 	</a>
+	<slot />
 </div>
