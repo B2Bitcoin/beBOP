c87276879ff439bf6f93a0d73cb86ca9471d084b
Agiltoo Agiltoo <ithiame@agiltoo.fr>
2023-06-09 17:13:19 +0000

    Template the challenge gauge #107

--- src/routes/challenges/+page.svelte
+++ src/routes/challenges/+page.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { marked } from 'marked';
-	import Picture from '$lib/components/Picture.svelte';
-	import PriceTag from '$lib/components/PriceTag.svelte';
-	import { applyAction, enhance } from '$app/forms';
-	import IconInfo from '$lib/components/icons/IconInfo.svelte';
-	import GoalProgress from '$lib/components/GoalProgress.svelte';
-	import { productAddedToCart } from '$lib/stores/productAddedToCart';
-	import { invalidate } from '$app/navigation';
-	import { UrlDependency } from '$lib/types/UrlDependency';
-
 	export let data;
 </script>
 
