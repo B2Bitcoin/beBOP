19f4a374d7a951cb0d0b0da8c958aeae500b41b0
coyotte508 <coyotte508@gmail.com>
2023-06-15 23:32:28 +0200

    🏷️ Update challenge data model

--- src/lib/types/Challenge.ts
+++ src/lib/types/Challenge.ts
@@ -4,16 +4,24 @@ import type { Timestamps } from './Timestamps';
 export interface Challenge extends Timestamps {
 	_id: string;
 	name: string;
+
 	/* If empty, works on all products */
-	productId: string[];
+	productIds: string[];
+
 	goal: {
-		currency: Currency;
+		currency?: Currency;
 		amount: number;
 	};
 
-	progress: {
-		currency: Currency;
-		amount: number;
-	};
+	/**
+	 * totalProducts: The goal is to sell a certain number of products
+	 */
+	mode: 'totalProducts' | 'moneyAmount';
+
+	recurring: false | 'monthly';
+
+	progress: number;
+
+	beginsAt?: Date;
 	endsAt: Date;
 }
