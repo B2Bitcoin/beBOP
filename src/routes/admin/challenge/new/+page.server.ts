193208de3089e47dfae5bb360205b1d2e94eaa88
zizaba <133223221+zizaba@users.noreply.github.com>
2023-06-15 22:29:24 +0000

    Update src/routes/admin/challenge/new/+page.server.ts
    Co-authored-by: Eliott C. <coyotte508@gmail.com>

--- src/routes/admin/challenge/new/+page.server.ts
+++ src/routes/admin/challenge/new/+page.server.ts
@@ -8,7 +8,7 @@ export const actions: Actions = {
 	default: async function ({ request }) {
 		const data = await request.formData();
 
-		const productId = [''];
+		const productIds: string[] = [];
 		const { name, goalAmount, mode, endsAt } = z
 			.object({
 				name: z.string().min(1).max(MAX_NAME_LIMIT),
