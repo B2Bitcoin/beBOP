1c3b593b0e089f13415ee33f337fd0dc37ed7e14
zizaba <133223221+zizaba@users.noreply.github.com>
2023-06-21 23:50:26 +0000

    Manag challenge gauge (#199)
    Co-authored-by: Abdou Aziz Ba <baabdouaziz3@gmail.com>
    Co-authored-by: Eliott C. <coyotte508@gmail.com>

---
+++ src/routes/admin/challenge/[id]/+page.server.ts
@@ -0,0 +1,70 @@
+import { collections } from '$lib/server/database.js';
+import { MAX_NAME_LIMIT } from '$lib/types/Product.js';
+import { error, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
+
+export async function load({ params }) {
+	const challenge = await collections.challenges.findOne({
+		_id: params.id
+	});
+
+	if (!challenge) {
+		throw error(404, 'Challenge not found');
+	}
+
+	const beginsAt = challenge.beginsAt?.toJSON().slice(0, 10);
+	const endsAt = challenge.endsAt.toJSON().slice(0, 10);
+
+	return {
+		challenge,
+		beginsAt,
+		endsAt
+	};
+}
+
+export const actions = {
+	update: async function ({ request, params }) {
+		const challenge = await collections.challenges.findOne({
+			_id: params.id
+		});
+
+		if (!challenge) {
+			throw error(404, 'Challenge not found');
+		}
+
+		const data = await request.formData();
+
+		const { name, goalAmount, beginsAt, endsAt } = z
+			.object({
+				name: z.string().min(1).max(MAX_NAME_LIMIT),
+				// productId: z.string().array(),
+				goalAmount: z.number({ coerce: true }).int().positive(),
+				beginsAt: z.date({ coerce: true }),
+				endsAt: z.date({ coerce: true })
+			})
+			.parse(Object.fromEntries(data));
+
+		await collections.challenges.updateOne(
+			{
+				_id: challenge._id
+			},
+			{
+				$set: {
+					name,
+					'goal.amount': goalAmount,
+					beginsAt,
+					endsAt,
+					updatedAt: new Date()
+				}
+			}
+		);
+	},
+
+	delete: async function ({ params }) {
+		await collections.challenges.deleteOne({
+			_id: params.id
+		});
+
+		throw redirect(303, '/admin/challenge');
+	}
+};
---
+++ src/routes/admin/challenge/[id]/+page.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { MAX_NAME_LIMIT } from '$lib/types/Product';
+	import { upperFirst } from '$lib/utils/upperFirst';
+
+	export let data;
+
+	let beginsAt = data.beginsAt;
+	let endsAt = data.endsAt;
+	let endsAtElement: HTMLInputElement;
+
+	function checkForm(event: SubmitEvent) {
+		if (endsAt < beginsAt) {
+			endsAtElement.setCustomValidity('End date must be after beginning date');
+			endsAtElement.reportValidity();
+			event.preventDefault();
+			return;
+		} else {
+			endsAtElement.setCustomValidity('');
+		}
+	}
+
+	function confirmDelete(event: Event) {
+		if (!confirm('Would you like to delete this Challenge?')) {
+			event.preventDefault();
+		}
+	}
+</script>
+
+<h1 class="text-3xl">Edit a challenge</h1>
+
+<form method="post" enctype="multipart/form-data" class="flex flex-col gap-4" on:submit={checkForm}>
+	<label class="form-label">
+		Challenge name
+		<input
+			class="form-input"
+			type="text"
+			maxlength={MAX_NAME_LIMIT}
+			name="name"
+			value={data.challenge.name}
+			placeholder="Challenge name"
+			required
+		/>
+	</label>
+
+	<label class="form-label">
+		Mode
+		<select class="form-input" name="mode" bind:value={data.challenge.mode} disabled>
+			{#each ['moneyAmount', 'totalProducts'] as option}
+				<option value={option}>{upperFirst(option)}</option>
+			{/each}
+		</select>
+	</label>
+
+	<label class="form-label">
+		Goal
+		<input
+			class="form-input"
+			type="number"
+			name="goalAmount"
+			value={data.challenge.goal.amount}
+			placeholder={data.challenge.mode === 'moneyAmount' ? 'Amount (SAT)' : 'Quantity'}
+			required
+		/>
+	</label>
+
+	<div class="flex flex-wrap gap-4">
+		<label class="form-label">
+			Beginning date
+
+			<input class="form-input" type="date" name="beginsAt" bind:value={beginsAt} required />
+		</label>
+	</div>
+
+	<div class="flex flex-wrap gap-4">
+		<label class="form-label">
+			Ending date
+
+			<input
+				class="form-input"
+				type="date"
+				required
+				name="endsAt"
+				bind:value={endsAt}
+				bind:this={endsAtElement}
+				on:input={() => endsAtElement?.setCustomValidity('')}
+			/>
+		</label>
+	</div>
+
+	<div class="flex flex-row justify-between gap-2">
+		<input type="submit" class="btn btn-blue text-white" formaction="?/update" value="Update" />
+		<a href="/{data.challenge._id}" class="btn btn-gray">View</a>
+
+		<input
+			type="submit"
+			class="btn btn-red text-white ml-auto"
+			formaction="?/delete"
+			value="Delete"
+			on:click={confirmDelete}
+		/>
+	</div>
+</form>
