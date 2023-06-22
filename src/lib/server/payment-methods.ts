3db6c0a10596e4d7e6e79fdad06170ddae8288d5
Eliott C <coyotte508@gmail.com>
2023-06-23 00:24:14 +0200

    🩹 Fix payment methods on checkout page (#205)

--- src/lib/server/payment-methods.ts
+++ src/lib/server/payment-methods.ts
@@ -3,7 +3,7 @@ import { isLightningConfigured } from './lightning';
 import { runtimeConfig } from './runtime-config';
 
 export const paymentMethods = () => [
-	...(isBitcoinConfigured ? (['bitcoin'] as const) : []),
 	...(isLightningConfigured ? (['lightning'] as const) : []),
+	...(isBitcoinConfigured ? (['bitcoin'] as const) : []),
 	...(runtimeConfig.enableCashSales ? (['cash'] as const) : [])
 ];
