c6e7b1272b95de0a4eed0a65c711507443cc276b
Eliott C <coyotte508@gmail.com>
2023-06-28 23:09:59 +0200

    📱 Responsive menu (#245)

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+/// <reference types="@sveltejs/kit" />
+/// <reference types="unplugin-icons/types/svelte" />
+
 declare global {
 	namespace App {
 		interface Error {
