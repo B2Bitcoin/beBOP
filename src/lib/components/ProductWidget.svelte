7831489fc67889a556258ecd995ad5cbfdbd5680
unknown <ithiame@agiltoo.fr>
2023-07-04 17:00:18 +0000

    As a bootik owner, I want my product widget to have some display options #103

--- src/lib/components/ProductWidget.svelte
+++ src/lib/components/ProductWidget.svelte
@@ -141,8 +141,12 @@
 			<div class="flex flex-row justify-start -mt-6 -ml-6">
 				<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2 text-sm" />
 			</div>
-			<a href="/product/{product._id}">
-				<PictureComponent {picture} sizes="800px" class="object-contain max-h-[250px] max-w-auto" />
+			<a href="/product/{product._id}" class="-ml-6">
+				<PictureComponent
+					{picture}
+					sizes="800px"
+					class="object-contain max-h-[264px] max-w-[264px]"
+				/>
 			</a>
 		</div>
 
@@ -220,6 +224,98 @@
 					</form>
 				</div>
 			{/if}
+		</div>
+	</div>
+{:else if displayOption === 'img-3'}
+	<div
+		class="relative mx-auto max-w-[800px] bg-gray-240 flex flex-row gap-4 p-6 rounded {className}"
+	>
+		<div class="flex flex-col gap-2">
+			<div class="flex flex-col gap-2">
+				<a href="/product/{product._id}" class="flex flex-col">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
+
+				<div class="grow" />
+
+				<div class="flex flex-row">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
+			</div>
+			<a href="/product/{product._id}" class="flex flex-col">
+				<p class="mt-2 text-gray-800">
+					{product.shortDescription}
+				</p>
+			</a>
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+		<div class="flex flex-col">
+			<div class="flex flex-row justify-end -mt-6 -mr-6">
+				<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2 text-sm" />
+			</div>
+			<a href="/product/{product._id}" class="-mr-6">
+				<PictureComponent
+					{picture}
+					sizes="800px"
+					class="object-contain max-h-[264px] max-w-[264px]"
+				/>
+			</a>
 		</div>
 	</div>
 {:else if displayOption === 'img-4'}
