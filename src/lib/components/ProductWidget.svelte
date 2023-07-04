22f90aebc5d835848b84fbb943f167a76275dcf5
unknown <ithiame@agiltoo.fr>
2023-07-04 16:07:37 +0000

    As a bootik owner, I want my product widget to have some display options #103

--- src/lib/components/ProductWidget.svelte
+++ src/lib/components/ProductWidget.svelte
@@ -26,6 +26,7 @@
 	export let exchangeRate = 0;
 	let className = '';
 	export { className as class };
+	export let displayOption = 'img-0';
 
 	let loading = false;
 
@@ -42,91 +43,438 @@
 
 	$: canAddToCart =
 		!product.availableDate || product.availableDate <= new Date() || !!product.preorder;
+	$: baseClasses = 'relative mx-auto max-w-[800px] bg-gray-240 flex flex-col gap-4 p-6 rounded';
 </script>
 
-<div class="relative mx-auto max-w-[800px] bg-gray-240 flex flex-col gap-4 p-6 rounded {className}">
-	<div class="flex flex-row justify-end -mt-6 -mr-6">
-		<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2" />
-	</div>
-	<div class="flex flex-col text-center">
-		<a href="/product/{product._id}" class="flex flex-col items-center">
-			<PictureComponent {picture} sizes="800px" class="object-contain max-h-[250px] max-w-full" />
-		</a>
-	</div>
-
-	<div class="flex flex-col">
-		<div class="flex flex-row">
-			<a href="/product/{product._id}" class="flex flex-col items-center">
-				<h2 class="text-2xl">{product.name}</h2>
-			</a>
-
-			<div class="grow" />
-
-			<div class="flex flex-row items-end justify-center">
-				<PriceTag
-					amount={product.price.amount}
-					currency={product.price.currency}
-					class="text-2xl text-gray-800"
-				/>
-				&nbsp; ~ &nbsp;
-				<PriceTag
-					class="text-base text-gray-600"
-					amount={product.price.amount}
-					currency={product.price.currency}
-					{exchangeRate}
-					convertedTo="EUR"
-				/>
-			</div>
+{#if displayOption === 'img-1'}
+	<div class="{baseClasses} {className}">
+		<div class="flex flex-row justify-end -mt-6 -mr-6">
+			<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2" />
 		</div>
-		<a href="/product/{product._id}" class="flex flex-col">
-			<p class="mt-2 text-gray-800">
-				{product.shortDescription}
-			</p>
-		</a>
-		{#if canAddToCart}
-			<div class="flex flex-row items-end justify-end">
-				<form
-					method="post"
-					class="contents"
-					use:enhance={() => {
-						loading = true;
-						return async ({ result }) => {
-							loading = false;
-							if (result.type === 'error') {
-								return await applyAction(result);
-							}
+		<div class="flex flex-col text-center">
+			<a href="/product/{product._id}" class="flex flex-col items-center">
+				<PictureComponent {picture} sizes="800px" class="object-contain max-h-[348px] max-w-full" />
+			</a>
+		</div>
 
-							await invalidate(UrlDependency.Cart);
-							addToCart();
-							// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
-							//document.body.scrollIntoView();
-						};
-					}}
-				>
-					<div class="relative">
-						<button
-							type="submit"
-							value="Add to cart"
-							disabled={loading}
-							formaction="/product/{product._id}?/addToCart"
-							class="btn btn-gray"
-						>
-							Add to cart
-						</button>
+		<div class="flex flex-col">
+			<div class="flex flex-row">
+				<a href="/product/{product._id}" class="flex flex-col items-center">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
 
-						{#if $productAddedToCart && $productAddedToCart.widget === widget}
-							<Popup>
-								<ProductAddedToCart
-									class="w-[562px] max-w-full"
-									on:dismiss={() => ($productAddedToCart = null)}
-									product={$productAddedToCart.product}
-									picture={$productAddedToCart.picture}
-								/>
-							</Popup>
-						{/if}
-					</div>
-				</form>
+				<div class="grow" />
+
+				<div class="flex flex-row items-end justify-center">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
 			</div>
-		{/if}
+			<a href="/product/{product._id}" class="flex flex-col">
+				<p class="mt-2 text-gray-800">
+					{product.shortDescription}
+				</p>
+			</a>
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
 	</div>
-</div>
+{:else if displayOption === 'img-2'}
+	<div
+		class="relative mx-auto max-w-[800px] bg-gray-240 flex flex-row gap-4 p-6 rounded {className}"
+	>
+		<div class="flex flex-col">
+			<div class="flex flex-row justify-start -mt-6 -ml-6">
+				<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2 text-sm" />
+			</div>
+			<a href="/product/{product._id}">
+				<PictureComponent {picture} sizes="800px" class="object-contain max-h-[250px] max-w-auto" />
+			</a>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<div class="flex flex-col gap-2">
+				<a href="/product/{product._id}" class="flex flex-col">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
+
+				<div class="grow" />
+
+				<div class="flex flex-row">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
+			</div>
+			<a href="/product/{product._id}" class="flex flex-col">
+				<p class="mt-2 text-gray-800">
+					{product.shortDescription} Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+					iusto quis ad? Ipsam recusandae saepe ipsa, nobis id excepturi delectus, ullam laboriosam fuga
+					illo architecto, dicta voluptatem maxime rerum adipisci?
+				</p>
+			</a>
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+	</div>
+{:else if displayOption === 'img-4'}
+	<div class="relative mx-auto w-264px bg-gray-240 flex flex-col gap-4 p-6 rounded {className}">
+		<div class="flex flex-col">
+			<div class="flex flex-row justify-end -mt-6 -mr-6">
+				<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2 text-sm" />
+			</div>
+			<a href="/product/{product._id}" class="-mx-6">
+				<PictureComponent {picture} sizes="800px" class="object-contain" />
+			</a>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<div class="flex flex-col gap-2">
+				<a href="/product/{product._id}" class="flex flex-col">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
+
+				<div class="grow" />
+
+				<div class="flex flex-row">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
+			</div>
+
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+	</div>
+{:else if displayOption === 'txt-1'}
+	<div class="{baseClasses} {className}">
+		<div class="flex flex-row justify-end -mt-6 -mr-6">
+			<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2" />
+		</div>
+		<div class="flex flex-col text-center">
+			<a href="/product/{product._id}" class="flex flex-col items-center">
+				<PictureComponent {picture} sizes="800px" class="object-contain max-h-[174px] max-w-full" />
+			</a>
+		</div>
+
+		<div class="flex flex-col">
+			<div class="flex flex-row">
+				<a href="/product/{product._id}" class="flex flex-col items-center">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
+
+				<div class="grow" />
+
+				<div class="flex flex-row items-end justify-center">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
+			</div>
+
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+	</div>
+{:else}
+	<div class="{baseClasses} {className}">
+		<div class="flex flex-row justify-end -mt-6 -mr-6">
+			<ProductType {product} class="last:rounded-tr first:rounded-bl pl-2" />
+		</div>
+		<div class="flex flex-col text-center">
+			<a href="/product/{product._id}" class="flex flex-col items-center">
+				<PictureComponent {picture} sizes="800px" class="object-contain max-h-[174px] max-w-full" />
+			</a>
+		</div>
+
+		<div class="flex flex-col">
+			<div class="flex flex-row">
+				<a href="/product/{product._id}" class="flex flex-col items-center">
+					<h2 class="text-2xl">{product.name}</h2>
+				</a>
+
+				<div class="grow" />
+
+				<div class="flex flex-row items-end justify-center">
+					<PriceTag
+						amount={product.price.amount}
+						currency={product.price.currency}
+						class="text-2xl text-gray-800"
+					/>
+					&nbsp; ~ &nbsp;
+					<PriceTag
+						class="text-base text-gray-600"
+						amount={product.price.amount}
+						currency={product.price.currency}
+						{exchangeRate}
+						convertedTo="EUR"
+					/>
+				</div>
+			</div>
+			<a href="/product/{product._id}" class="flex flex-col">
+				<p class="mt-2 text-gray-800">
+					{product.shortDescription}
+				</p>
+			</a>
+			{#if canAddToCart}
+				<div class="flex flex-row items-end justify-end">
+					<form
+						method="post"
+						class="contents"
+						use:enhance={() => {
+							loading = true;
+							return async ({ result }) => {
+								loading = false;
+								if (result.type === 'error') {
+									return await applyAction(result);
+								}
+
+								await invalidate(UrlDependency.Cart);
+								addToCart();
+								// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+								//document.body.scrollIntoView();
+							};
+						}}
+					>
+						<div class="relative">
+							<button
+								type="submit"
+								value="Add to cart"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="btn btn-gray"
+							>
+								Add to cart
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					</form>
+				</div>
+			{/if}
+		</div>
+	</div>
+{/if}
--- src/routes/product/[id]/widget/+page.svelte
+++ src/routes/product/[id]/widget/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import ProductWidget from '$lib/components/ProductWidget.svelte';
+	import { page } from '$app/stores';
 
 	export let data;
+	const img = $page.url.searchParams.get('img');
+	const txt = $page.url.searchParams.get('txt');
 </script>
 
-<ProductWidget product={data.product} picture={data.picture} exchangeRate={data.exchangeRate} />
+<ProductWidget
+	product={data.product}
+	picture={data.picture}
+	exchangeRate={data.exchangeRate}
+	displayOption={img ? 'img-' + img : txt ? 'txt-' + txt : 'img-0'}
+/>
