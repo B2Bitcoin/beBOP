7fc1c30da013d4a70421898ee7a2b7fdd0590aea
Abdou Aziz Ba <baabdouaziz3@gmail.com>
2023-08-10 00:45:43 +0000

    my cabinet implementation

---
+++ src/routes/(app)/my-cabinet/+page.svelte
@@ -0,0 +1,194 @@
+<script>
+	import IconDownload from '$lib/components/icons/IconDownload.svelte';
+	import IconSuscription from '$lib/components/icons/IconSuscription.svelte';
+</script>
+
+<div class="items-center pt-8 mb-4 mx-auto max-w-7xl flex flex-col">
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-1-j0nzXL/format/100"
+				alt="My Cabinet 1"
+			/>
+			<div class="flex flex-col gap-2">
+				<span class="grow text-2xl">Procreate Brushes Set</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-green-500 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet A</span>
+					<IconSuscription />
+					<span class="text-yellow-500 ml-2">By suscription</span>
+				</div>
+				<span class="text-gray-550">List of product details, separated by comma.</span>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="text-gray-550">Download</a>
+			</div>
+		</div>
+	</div>
+	<hr class="border-gray-300 my-4 w-full" />
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-3-S79ZEz/format/100"
+				alt="My cabinet 2"
+			/>
+			<div class="flex flex-col gap-2 max-w-3xl">
+				<span class="grow text-2xl">Joel + Ellie Wallpaper!</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-blue-200 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet B</span>
+				</div>
+				<span class="text-gray-550"
+					>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+					consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span
+				>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="hidden sm:inline">Download</a>
+			</div>
+		</div>
+	</div>
+	<hr class="border-gray-300 my-4 w-full" />
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-2-xDvRi8/format/100"
+				alt="My cabinet 3"
+			/>
+			<div class="flex flex-col gap-2 max-w-3xl">
+				<span class="grow text-2xl">FEBRUARY PNG PACK!</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-purple-500 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet C</span>
+					<IconSuscription />
+					<span class="text-yellow-500 ml-2">By suscription</span>
+				</div>
+				<span class="text-gray-550"
+					>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+					consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span
+				>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="hidden sm:inline">Download</a>
+			</div>
+		</div>
+	</div>
+	<hr class="border-gray-300 my-4 w-full" />
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-1-j0nzXL/format/100"
+				alt="My Cabinet 1"
+			/>
+			<div class="flex flex-col gap-2">
+				<span class="grow text-2xl">Procreate Brushes Set</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-green-500 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet A</span>
+					<IconSuscription />
+					<span class="text-yellow-500 ml-2">By suscription</span>
+				</div>
+				<span class="text-gray-550">List of product details, separated by comma.</span>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="text-gray-550">Download</a>
+			</div>
+		</div>
+	</div>
+	<hr class="border-gray-300 my-4 w-full" />
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-3-S79ZEz/format/100"
+				alt="My cabinet 2"
+			/>
+			<div class="flex flex-col gap-2 max-w-3xl">
+				<span class="grow text-2xl">Joel + Ellie Wallpaper!</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-blue-200 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet B</span>
+				</div>
+				<span class="text-gray-550"
+					>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+					consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span
+				>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="hidden sm:inline">Download</a>
+			</div>
+		</div>
+	</div>
+	<hr class="border-gray-300 my-4 w-full" />
+	<div class="w-full flex items-center gap-6 px-6 grow">
+		<div class="flex gap-4">
+			<img
+				class="h-[80px] w-[70px]"
+				src="https://labookinerie.com/picture/raw/my-cabinet-2-xDvRi8/format/100"
+				alt="My cabinet 3"
+			/>
+			<div class="flex flex-col gap-2 max-w-3xl">
+				<span class="grow text-2xl">FEBRUARY PNG PACK!</span>
+				<div class="flex items-center">
+					<div class="w-2 h-2 m-2 bg-purple-500 rounded-full" />
+					<span class="text-gray-550 mr-4">Wallet C</span>
+					<IconSuscription />
+					<span class="text-yellow-500 ml-2">By suscription</span>
+				</div>
+				<span class="text-gray-550"
+					>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+					consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span
+				>
+			</div>
+		</div>
+		<span class="grow" />
+		<div class="flex gap-10">
+			<a href="/" class="underline text-blue-800">Give away a copy</a>
+			<div class="flex gap-2">
+				<IconDownload />
+				<a href="/" class="hidden sm:inline">Download</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.text-yellow-500 {
+		color: #e4c315;
+	}
+	.text-blue-800 {
+		color: #2271b1;
+	}
+	.bg-purple-500 {
+		background-color: #704aa0;
+	}
+</style>
