cef2936e5d618a0e33d65b1aa17d830fa1223c5a
ithiame <133223073+ithiame@users.noreply.github.com>
2023-09-06 10:12:31 +0000

    On product page, remove picture preview / selector for product with only 1 picture #403 (#431)

--- src/routes/(app)/admin/product/[id]/+page.svelte
+++ src/routes/(app)/admin/product/[id]/+page.svelte
@@ -5,6 +5,7 @@
 	import { MAX_NAME_LIMIT, MAX_SHORT_DESCRIPTION_LIMIT } from '$lib/types/Product';
 	import { CURRENCIES, SATOSHIS_PER_BTC } from '$lib/types/Currency';
 	import DeliveryFeesSelector from '$lib/components/DeliveryFeesSelector.svelte';
+	import { page } from '$app/stores';
 
 	export let data;
 
@@ -49,7 +50,32 @@
 	}
 </script>
 
-<h1 class="text-3xl">Edit a product</h1>
+<!-- <h1 class="text-3xl">Edit a product</h1> -->
+
+<ul
+	class="flex flex-wrap text-xl font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+>
+	<li class="mr-2">
+		<a
+			href="/admin/product/{data.product._id}"
+			class="{$page.url.pathname === '/admin/product/' + data.product._id
+				? 'inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500'
+				: 'inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300'} "
+			>Edit a product</a
+		>
+	</li>
+	{#if data.product.type === 'subscription'}
+		<li class="mr-2">
+			<a
+				href="/admin/product/{data.product._id}/subscribers"
+				class="{$page.url.pathname === '/admin/product/' + data.product._id + '/subscribers'
+					? 'inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500'
+					: 'inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300'} "
+				>Subscribers</a
+			>
+		</li>
+	{/if}
+</ul>
 
 <div class="flex flex-col">
 	<form method="post" class="flex flex-col gap-4" action="?/update" on:submit={checkForm}>
