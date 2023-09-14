8581c737329d0c54bb2024718c1b29e5507c768e
coyotte508 <coyotte508@gmail.com>
2023-09-14 23:08:03 +0200

    👔 Use satoshi as intermediate currency

--- src/lib/utils/sumCurrency.ts
+++ src/lib/utils/sumCurrency.ts
@@ -1,6 +1,4 @@
-import { currencies } from '$lib/stores/currencies';
 import type { Currency } from '$lib/types/Currency';
-import { get } from 'svelte/store';
 import { toCurrency } from './toCurrency';
 import { sum } from './sum';
 
@@ -8,7 +6,7 @@ import { sum } from './sum';
  * Sum currencies, using the priceReferenceCurrency as intermediary
  */
 export function sumCurrency(to: Currency, items: Array<{ amount: number; currency: Currency }>) {
-	const refCurrency = get(currencies).priceReference;
+	const refCurrency = 'SAT'; //get(currencies).priceReference;
 
 	const amounts = items.map((item) => toCurrency(refCurrency, item.amount, item.currency));
 
