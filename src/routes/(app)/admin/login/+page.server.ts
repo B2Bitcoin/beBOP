2b6a93aaa643205c7f31e69d2b9236452e7dd19c
unknown <ithiame@agiltoo.fr>
2023-10-05 08:43:44 +0000

    Manage superadmin account login #461

--- src/routes/(app)/admin/login/+page.server.ts
+++ src/routes/(app)/admin/login/+page.server.ts
@@ -26,30 +26,20 @@ export const actions: Actions = {
 				memorize: data.get('memorize')
 			});
 		const authenticateUser = await collections.users.findOne({ login: login });
-		if (authenticateUser && bcryptjs.compareSync(password, authenticateUser.password)) {
+		if (authenticateUser && (await bcryptjs.compare(password, authenticateUser.password))) {
 			await collections.users.updateOne(
 				{ _id: authenticateUser._id },
 				{ $set: { updatedAt: new Date() } }
 			);
-			if (remember) {
-				await collections.sessions.insertOne({
-					_id: new ObjectId(),
-					userId: authenticateUser._id,
-					sessionId: locals.sessionId,
-					expiresAt: addSeconds(new Date(), memorize),
-					createdAt: new Date(),
-					updatedAt: new Date()
-				});
-			} else {
-				await collections.sessions.insertOne({
-					_id: new ObjectId(),
-					userId: authenticateUser._id,
-					sessionId: locals.sessionId,
-					expiresAt: addSeconds(new Date(), 3600),
-					createdAt: new Date(),
-					updatedAt: new Date()
-				});
-			}
+
+			await collections.sessions.insertOne({
+				_id: new ObjectId(),
+				userId: authenticateUser._id,
+				sessionId: locals.sessionId,
+				expiresAt: addSeconds(new Date(), remember ? memorize : 3600),
+				createdAt: new Date(),
+				updatedAt: new Date()
+			});
 
 			// Redirect to the admin dashboard upon successful login
 			throw redirect(303, `/admin`);
