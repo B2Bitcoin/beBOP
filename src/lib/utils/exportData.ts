b8d4bbd93bcbca38cef4b0b780ad9726ef975cf2
adrienBarb <adrien-barbier@hotmail.fr>
2023-10-10 17:05:11 +0200

    #269 : export collection

--- src/lib/utils/exportData.ts
+++ src/lib/utils/exportData.ts
@@ -3,9 +3,24 @@ import { collections } from '../server/database';
 export async function exportDatabase() {
 	try {
 		const challenges = await collections.challenges.find().toArray();
-		const users = await collections.users.find().toArray();
+		const cmsPages = await collections.cmsPages.find().toArray();
+		const digitalFiles = await collections.digitalFiles.find().toArray();
+		const orders = await collections.orders.find().toArray();
+		const pictures = await collections.pictures.find().toArray();
+		const products = await collections.products.find().toArray();
+		const runtimeConfig = await collections.runtimeConfig.find().toArray();
+		const paidSubscriptions = await collections.paidSubscriptions.find().toArray();
 
-		return { challenges, users };
+		return {
+			challenges,
+			cmsPages,
+			digitalFiles,
+			orders,
+			pictures,
+			products,
+			runtimeConfig,
+			paidSubscriptions
+		};
 	} catch (error) {
 		console.error('Error exporting database:', error);
 		throw error;
