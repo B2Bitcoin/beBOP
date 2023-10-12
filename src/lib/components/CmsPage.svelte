7137e7436824d404b95cfb8799ea73b619917b0b
coyotte508 <coyotte508@gmail.com>
2023-10-12 23:34:37 +0200

    ✨ Add "prose" style even on full-screen cms page

--- src/lib/components/CmsPage.svelte
+++ src/lib/components/CmsPage.svelte
@@ -24,21 +24,23 @@
 </svelte:head>
 
 {#if cmsPage.fullScreen}
-	{#each tokens as token}
-		{#if token.type === 'productWidget' && productById[token.slug]}
-			<ProductWidget
-				product={productById[token.slug]}
-				picture={pictureByProduct[token.slug]}
-				hasDigitalFiles={digitalFilesByProduct[token.slug] !== null}
-				displayOption={token.display}
-				class="not-prose my-5"
-			/>
-		{:else if token.type === 'challengeWidget' && challengeById[token.slug]}
-			<ChallengeWidget challenge={challengeById[token.slug]} class="my-5" />
-		{:else}
-			{@html token.raw}
-		{/if}
-	{/each}
+	<article class="prose max-w-full">
+		{#each tokens as token}
+			{#if token.type === 'productWidget' && productById[token.slug]}
+				<ProductWidget
+					product={productById[token.slug]}
+					picture={pictureByProduct[token.slug]}
+					hasDigitalFiles={digitalFilesByProduct[token.slug] !== null}
+					displayOption={token.display}
+					class="not-prose my-5"
+				/>
+			{:else if token.type === 'challengeWidget' && challengeById[token.slug]}
+				<ChallengeWidget challenge={challengeById[token.slug]} class="my-5" />
+			{:else}
+				{@html token.raw}
+			{/if}
+		{/each}
+	</article>
 {:else}
 	<main class="mx-auto max-w-7xl py-10 px-6">
 		<article class="w-full rounded-xl bg-white border-gray-300 border p-6">
