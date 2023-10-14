b7b06bd0790adb90971259955deb60076208296d
Eliott C <coyotte508@gmail.com>
2023-10-14 12:28:54 +0200

    ✨ Endpoint to dump priv keys + descriptors (#504)

--- src/lib/server/bitcoin.ts
+++ src/lib/server/bitcoin.ts
@@ -8,6 +8,8 @@ import { error } from '@sveltejs/kit';
 import { z } from 'zod';
 import { runtimeConfig } from './runtime-config';
 import { socksDispatcher } from 'fetch-socks';
+import { inspect } from 'node:util';
+import { filterUndef } from '$lib/utils/filterUndef';
 
 export const isBitcoinConfigured =
 	!!BITCOIN_RPC_URL && !!BITCOIN_RPC_PASSWORD && !!BITCOIN_RPC_USER;
@@ -28,21 +30,28 @@ type BitcoinCommand =
 	| 'listtransactions'
 	| 'listwallets'
 	| 'createwallet'
+	| 'listreceivedbyaddress'
+	| 'dumpprivkey'
+	| 'listdescriptors'
 	| 'getnewaddress'
 	| 'getbalance'
 	| 'getblockchaininfo';
 
-export async function bitcoinRpc(command: BitcoinCommand, params: unknown[]) {
+export async function bitcoinRpc(command: BitcoinCommand, params: unknown[], wallet?: string) {
 	let url = BITCOIN_RPC_URL;
 
 	if (!['listwallets', 'createwallet', 'getblockhaininfo'].includes(command)) {
 		const wallets = await listWallets();
 
+		if (wallet && !wallets.includes(wallet)) {
+			throw error(400, `Wallet ${wallet} does not exist`);
+		}
+
 		if (wallets.length > 1) {
-			const wallet = wallets.includes(runtimeConfig.bitcoinWallet)
-				? runtimeConfig.bitcoinWallet
-				: wallets[0];
-			url = `${url.endsWith('/') ? url.slice(0, -1) : url}/wallet/${wallet}`;
+			const selectedWallet =
+				wallet ||
+				(wallets.includes(runtimeConfig.bitcoinWallet) ? runtimeConfig.bitcoinWallet : wallets[0]);
+			url = `${url.endsWith('/') ? url.slice(0, -1) : url}/wallet/${selectedWallet}`;
 		}
 	}
 
@@ -160,6 +169,76 @@ export async function getBlockchainInfo() {
 	const json = await response.json();
 	return z.object({ result: z.object({ blocks: z.number(), chain: z.string() }) }).parse(json)
 		.result;
+}
+
+export async function listDescriptors(wallet: string) {
+	const response = await bitcoinRpc('listdescriptors', [true], wallet);
+
+	if (!response.ok) {
+		console.error(await response.text());
+		throw error(500, 'Could not list wallet descriptors');
+	}
+
+	const json = await response.json();
+	return z
+		.object({ result: z.object({ descriptors: z.array(z.object({ desc: z.string() })) }) })
+		.parse(json).result;
+}
+
+export async function dumpWalletInfo(wallet: string): Promise<{
+	descriptors?: Array<{ desc: string }>;
+	privKeys?: Array<{
+		address: string;
+		privKey: string;
+		balance: number;
+	}>;
+}> {
+	try {
+		const descriptors = await listDescriptors(wallet);
+
+		if (descriptors.descriptors.length > 0) {
+			return descriptors;
+		}
+	} catch {}
+
+	const response = await bitcoinRpc('listreceivedbyaddress', [0, false, false, '', true], wallet);
+
+	if (!response.ok) {
+		console.error(await response.text());
+		throw error(500, 'Could not list wallet addresses');
+	}
+
+	const json = await response.json();
+	const { result: addresses } = z
+		.object({
+			result: z.array(z.object({ address: z.string(), amount: z.number() }))
+		})
+		.parse(json);
+
+	const privKeys = filterUndef(
+		await Promise.all(
+			addresses.map(async ({ address, amount }) => {
+				const response = await bitcoinRpc('dumpprivkey', [address], wallet);
+
+				if (!response.ok) {
+					console.error(await response.text());
+					throw error(500, 'Could not dump private key for address: ' + address);
+				}
+
+				const privKey = z.object({ result: z.string() }).parse(await response.json()).result;
+
+				return {
+					address,
+					privKey,
+					balance: amount
+				};
+			})
+		)
+	);
+
+	return {
+		privKeys
+	};
 }
 
 export type BitcoinTransaction = Awaited<ReturnType<typeof listTransactions>>[number];
