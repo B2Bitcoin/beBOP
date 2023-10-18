92903dc199ebc6ed555df0d1329babd2761ab738
unknown <ithiame@agiltoo.fr>
2023-10-18 11:49:27 +0000

    Browse as / Login / Session link : removing user creation

--- src/routes/(app)/customer/login/[token]/+page.server.ts
+++ src/routes/(app)/customer/login/[token]/+page.server.ts
@@ -1,13 +1,10 @@
 import { collections } from '$lib/server/database';
 import { error } from '@sveltejs/kit';
 
-import type { User } from '$lib/types/User';
-import { ObjectId } from 'mongodb';
-import { addMinutes } from 'date-fns';
 import type { SetRequired } from 'type-fest';
 import type { Session } from '$lib/types/session';
 
-export async function load({ params, locals }) {
+export async function load({ params }) {
 	const session = await collections.sessions.findOne<SetRequired<Session, 'authLink'>>({
 		'authLink.token': params.token
 	});
