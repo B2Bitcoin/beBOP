f1a9f050c32a8d3f78ac897a07659a77cecb1617
coyotte508 <coyotte508@gmail.com>
2023-10-19 03:10:52 +0200

    🐛 Better handling of max- classes

--- src/lib/components/Picture.svelte
+++ src/lib/components/Picture.svelte
@@ -7,16 +7,20 @@
 	export let style = '';
 
 	let matchedWidth: number | null = null;
+	let maxWidth: boolean;
 	let matchedHeight: number | null = null;
+	let maxHeight: boolean;
 
 	$: {
 		const match4px = className.match(/(\s|^)(max-)?w-(\d+)(\s|$)/);
 		if (match4px) {
 			matchedWidth = parseInt(match4px[3]) * 4;
+			maxWidth = match4px[2] === 'max-';
 		} else {
 			const matchPx = className.match(/(\s|^)(max-)?w-\[(\d+)px](\s|$)/);
 			if (matchPx) {
 				matchedWidth = parseInt(matchPx[3]);
+				maxWidth = matchPx[2] === 'max-';
 			} else {
 				matchedWidth = null;
 			}
@@ -27,10 +31,12 @@
 		const match4px = className.match(/(\s|^)(max-)?h-(\d+)(\s|$)/);
 		if (match4px) {
 			matchedHeight = parseInt(match4px[3]) * 4;
+			maxHeight = match4px[2] === 'max-';
 		} else {
 			const matchPx = className.match(/(\s|^)(max-)?h-\[(\d+)px](\s|$)/);
 			if (matchPx) {
 				matchedHeight = parseInt(matchPx[3]);
+				maxHeight = matchPx[2] === 'max-';
 			} else {
 				matchedHeight = null;
 			}
@@ -39,11 +45,12 @@
 
 	let computedWidth: number | null = null;
 	let computedHeight: number | null = null;
+	let maxComputedWidth: number | null = null;
 
 	$: {
-		if (matchedWidth !== null) {
+		if (matchedWidth !== null && !maxWidth) {
 			computedWidth = null;
-		} else if (matchedHeight !== null && picture) {
+		} else if (matchedHeight !== null && !maxHeight && picture) {
 			computedWidth = Math.round(
 				(matchedHeight / picture.storage.original.height) * picture.storage.original.width
 			);
@@ -53,9 +60,9 @@
 	}
 
 	$: {
-		if (matchedHeight !== null) {
+		if (matchedHeight !== null && !maxHeight) {
 			computedHeight = null;
-		} else if (matchedWidth !== null && picture) {
+		} else if (matchedWidth !== null && !maxWidth && picture) {
 			computedHeight = Math.round(
 				(matchedWidth / picture.storage.original.width) * picture.storage.original.height
 			);
@@ -64,9 +71,32 @@
 		}
 	}
 
+	// If max width or max hegiht is set, while width/height are not set. Use the more restrictive computed width.
+	$: {
+		if ((matchedWidth !== null && !maxWidth) || (matchedHeight !== null && !maxHeight)) {
+			maxComputedWidth = null;
+		} else {
+			let max1: number | null = null;
+			let max2: number | null = matchedWidth;
+
+			if (matchedHeight !== null && picture) {
+				max1 = Math.round(
+					(matchedHeight / picture.storage.original.height) * picture.storage.original.width
+				);
+			}
+			if (max1 !== null || max2 !== null) {
+				maxComputedWidth = Math.min(max1 ?? Infinity, max2 ?? Infinity);
+			} else {
+				maxComputedWidth = null;
+			}
+		}
+	}
+
 	$: computedStyle = `${computedWidth !== null ? `width: ${computedWidth}px;` : ''} ${
 		computedHeight !== null ? `height: ${computedHeight}px;` : ''
 	} ${style};`;
+	$: sizeHint =
+		matchedWidth !== null && !maxWidth ? matchedWidth : computedWidth ?? maxComputedWidth;
 </script>
 
 {#if picture}
@@ -77,9 +107,7 @@
 		srcset={picture.storage.formats
 			.map((format) => `/picture/raw/${picture?._id}/format/${format.width} ${format.width}w`)
 			.join(', ')}
-		sizes={matchedWidth ?? computedWidth !== null
-			? `${matchedWidth ?? computedWidth}px`
-			: undefined}
+		sizes={sizeHint !== null ? `${sizeHint}px` : undefined}
 		class={className}
 		style={computedStyle}
 		{...$$restProps}
