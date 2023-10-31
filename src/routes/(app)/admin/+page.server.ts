449a316fb3b5d22084a7b3786b369b0aef51d4b4
coyotte508 <coyotte508@gmail.com>
2023-10-31 22:22:03 +0100

    ✨ Add admin home

--- src/routes/(app)/admin/+page.server.ts
+++ src/routes/(app)/admin/+page.server.ts
@@ -1,23 +1,12 @@
+import { CUSTOMER_ROLE_ID } from '$lib/types/User.js';
 import { error, redirect } from '@sveltejs/kit';
-import { adminLinks } from './adminLinks.js';
-import { isAllowedOnPage } from '$lib/types/Role.js';
 
-export async function load({ parent, locals }) {
+export async function load({ locals }) {
 	if (!locals.user) {
 		throw redirect(303, '/admin/login');
 	}
 
-	const role = (await parent()).role;
-
-	if (!role) {
-		throw error(404, 'Role not found');
+	if (locals.user.role === CUSTOMER_ROLE_ID) {
+		throw error(403, 'This page is only accessible to administrators');
 	}
-
-	for (const option of adminLinks) {
-		if (isAllowedOnPage(role, option.href, 'read')) {
-			throw redirect(303, option.href);
-		}
-	}
-
-	throw error(403, 'Your account does not have access to any of the admin pages');
 }
