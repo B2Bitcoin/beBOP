2352a6fc804412c977b1422843feaf9182a4bf26
unknown <ithiame@agiltoo.fr>
2023-11-02 10:30:42 +0000

    Create a caroussel widget #491: Using flowbite Carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 	theme: {
 		extend: {},
 		colors: {
@@ -61,8 +64,8 @@ module.exports = {
 	},
 	/* To style checkboxes with form-checkbox */
 	plugins: [
-			require('@tailwindcss/forms'),
-			require('@tailwindcss/typography'),
-			require('flowbite/plugin')
-		]
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		require('flowbite/plugin')
+	]
 };
