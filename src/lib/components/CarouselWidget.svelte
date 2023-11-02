c7859dbbf8abd2bce730bbd48af091c640105f0d
unknown <ithiame@agiltoo.fr>
2023-11-02 10:37:23 +0000

    Create a caroussel widget #491: Using flowbite Carousel

--- src/lib/components/CarouselWidget.svelte
+++ src/lib/components/CarouselWidget.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { Carousel } from 'flowbite-svelte';
 	import type { Picture } from '$lib/types/Picture';
+	import { scale } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
 
 	export let pictures: Picture[];
-	export let autoplay: number | 30000;
+	export let autoplay: number;
 
 	let images = pictures.map((picture) => ({
 		alt: picture.name,
@@ -13,10 +15,12 @@
 		title: picture.name,
 		url: picture.slider?.url
 	}));
+
+	const scaleAnimation = (x: any) => scale(x, { duration: 500, easing: quintOut });
 </script>
 
 <div class="mx-auto max-w-4xl">
-	<Carousel {images} duration={autoplay} let:Indicators>
+	<Carousel {images} duration={autoplay} transition={scaleAnimation} let:Indicators>
 		<a slot="slide" href={images[index]?.url} target="_blank" let:Slide let:index>
 			<Slide image={images[index]} />
 		</a>
