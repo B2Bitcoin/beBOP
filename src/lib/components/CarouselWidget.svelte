ef45d35cc5467a03c65a08956ca5c93b03717717
unknown <ithiame@agiltoo.fr>
2023-11-02 10:51:23 +0000

    Create a caroussel widget #491: Using flowbite Carousel

--- src/lib/components/CarouselWidget.svelte
+++ src/lib/components/CarouselWidget.svelte
@@ -11,13 +11,19 @@
 			.map((format) => `/picture/raw/${picture?._id}/format/${format.width} ${format.width}w`)
 			.join(', '),
 		title: picture.name,
-		url: picture.slider?.url
+		slide: picture.slider
 	}));
 </script>
 
 <div class="mx-auto max-w-4xl">
 	<Carousel {images} duration={autoplay} transition={null} let:Indicators>
-		<a slot="slide" href={images[index]?.url} target="_blank" let:Slide let:index>
+		<a
+			slot="slide"
+			href={images[index].slide?.url}
+			target={images[index].slide?.openNewTab ? '_blank' : '_self'}
+			let:Slide
+			let:index
+		>
 			<Slide image={images[index]} />
 		</a>
 		<Indicators />
