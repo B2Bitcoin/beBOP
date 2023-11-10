985c0eb58a52dbe559fd2895c3c1ea47221859ff
unknown <ithiame@agiltoo.fr>
2023-11-10 15:51:23 +0000

    use tag widget on /tag/[id]

--- src/routes/(app)/tag/[id]/+page.svelte
+++ src/routes/(app)/tag/[id]/+page.svelte
@@ -1,46 +1,16 @@
 <script lang="ts">
-	import VariationFiveTemplateWidget from '$lib/components/TagWidget/VariationFiveTemplateWidget.svelte';
-	import VariationFourTemplateWidget from '$lib/components/TagWidget/VariationFourTemplateWidget.svelte';
-	import VariationOneTemplateWidget from '$lib/components/TagWidget/VariationOneTemplateWidget.svelte';
-	import VariationSixTemplateWidget from '$lib/components/TagWidget/VariationSixTemplateWidget.svelte';
-	import VariationThreeTemplateWidget from '$lib/components/TagWidget/VariationThreeTemplateWidget.svelte';
-	import VariationTwoTemplateWidget from '$lib/components/TagWidget/VariationTwoTemplateWidget.svelte';
+	import TagWidget from '$lib/components/TagWidget.svelte';
 
 	export let data;
 </script>
 
 <main class="mx-auto max-w-7xl py-10 px-6">
 	<article class="w-full rounded-xl bg-white border-gray-300 border py-3 px-3 flex flex-col gap-2">
-		<VariationOneTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'full')}
-			avatar={data.pictures.find((picture) => picture.tag?.type === 'avatar')}
-		/>
-
-		<VariationTwoTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'wide')}
-			class="not-prose"
-		/>
-		<VariationThreeTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'main')}
-			class="not-prose"
-		/>
-		<VariationFourTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'avatar')}
-			class="not-prose"
-		/>
-		<VariationFiveTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'slim')}
-			class="not-prose"
-		/>
-		<VariationSixTemplateWidget
-			tag={data.tag}
-			picture={data.pictures.find((picture) => picture.tag?.type === 'slim')}
-			class="not-prose"
-		/>
+		<TagWidget tag={data.tag} displayOption="var-1" pictures={data.pictures} />
+		<TagWidget tag={data.tag} displayOption="var-2" pictures={data.pictures} />
+		<TagWidget tag={data.tag} displayOption="var-3" pictures={data.pictures} />
+		<TagWidget tag={data.tag} displayOption="var-4" pictures={data.pictures} />
+		<TagWidget tag={data.tag} displayOption="var-5" pictures={data.pictures} />
+		<TagWidget tag={data.tag} displayOption="var-6" pictures={data.pictures} />
 	</article>
 </main>
