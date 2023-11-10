541205c7b65f4792b25d3037aef760f6dab520a8
coyotte508 <coyotte508@gmail.com>
2023-11-10 02:15:34 +0100

    🌐 Fix some translations

--- src/lib/components/Trans.svelte
+++ src/lib/components/Trans.svelte
@@ -1,36 +1,43 @@
 <script lang="ts">
 	import { useI18n } from '$lib/i18n';
 
-	useI18n();
+	const { t } = useI18n();
 
 	export let key: string;
 
 	$: tokens = getTokens(key);
 
 	function getTokens(key: string) {
+		let translation = t(key);
+		const props = Object.fromEntries(
+			[...translation.matchAll(/%{([^}]+)}/g)].map((match) => [match[1], match[0]])
+		);
+		// Alternative is fetching the raw translation from the messages store
+		translation = t(key, props);
 		const ret: Array<string | { key: string }> = [];
 
 		let index = 0;
 
 		do {
-			let nextIndex = key.indexOf('%{', index);
+			let nextIndex = translation.indexOf('%{', index);
 
 			if (nextIndex === -1) {
-				ret.push(key.slice(index));
+				ret.push(translation.slice(index));
 				break;
 			}
 
-			let end = key.indexOf('}', nextIndex);
+			let end = translation.indexOf('}', nextIndex);
 
 			if (end === -1) {
-				ret.push(key.slice(index));
+				ret.push(translation.slice(index));
 				break;
 			}
 
-			ret.push({ key: key.slice(nextIndex + 2, end) });
+			ret.push(translation.slice(index, nextIndex));
+			ret.push({ key: translation.slice(nextIndex + 2, end) });
 
 			index = end + 1;
-		} while (index < key.length);
+		} while (index < translation.length);
 
 		return ret;
 	}
