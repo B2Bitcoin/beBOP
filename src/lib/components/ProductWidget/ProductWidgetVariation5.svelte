a6d2e3bd56c85913b76bd4d93d677fcbca02960e
unknown <ithiame@agiltoo.fr>
2023-11-14 13:00:41 +0000

    add ctas, link to product/id and miniature fix

--- src/lib/components/ProductWidget/ProductWidgetVariation5.svelte
+++ src/lib/components/ProductWidget/ProductWidgetVariation5.svelte
@@ -3,6 +3,12 @@
 	import type { Picture } from '$lib/types/Picture';
 	import PictureComponent from '../Picture.svelte';
 	import { useI18n } from '$lib/i18n';
+	import Popup from '../Popup.svelte';
+	import { productAddedToCart } from '$lib/stores/productAddedToCart';
+	import ProductAddedToCart from '../ProductAddedToCart.svelte';
+	import { applyAction, enhance } from '$app/forms';
+	import { invalidate } from '$app/navigation';
+	import { UrlDependency } from '$lib/types/UrlDependency';
 
 	let className = '';
 	export { className as class };
@@ -20,8 +26,21 @@
 		| 'actionSettings'
 	>;
 	export let pictures: Picture[] | [];
-	const { t } = useI18n();
+	export let canAddToCart: boolean;
+
+	const widget = {};
+	function addToCart() {
+		$productAddedToCart = {
+			product,
+			quantity: 1,
+			picture: pictures[0],
+			widget
+		};
+	}
 	let loading = false;
+
+	const { t } = useI18n();
+	let pictureId = 0;
 </script>
 
 <div class="flex flex-col mx-auto rounded p-4 sm:flex-row sm:gap-2 {className}">
@@ -33,17 +52,49 @@
 			<a href="/product/{product._id}">
 				<p class="text-gray-600 mb-4">{product.shortDescription}</p>
 			</a>
-			<form method="post" class="contents">
+			<form
+				method="post"
+				class="contents"
+				use:enhance={() => {
+					loading = true;
+					return async ({ result }) => {
+						loading = false;
+						if (result.type === 'error') {
+							return await applyAction(result);
+						}
+
+						await invalidate(UrlDependency.Cart);
+						addToCart();
+						// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+						//document.body.scrollIntoView();
+					};
+				}}
+			>
 				<div class="flex flex-wrap gap-6 items-end">
-					<button
-						type="submit"
-						formaction="/product/{product._id}?/buy"
-						on:submit={() => (loading = true)}
-						disabled={loading}
-						class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1"
-					>
-						{t('product.cta.buy')}
-					</button>
+					{#if canAddToCart}
+						<div class="relative">
+							<button
+								type="submit"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1"
+							>
+								{t('product.cta.buy')}
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+										customPrice={$productAddedToCart.customPrice}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					{/if}
 					<a href="/product/{product._id}">
 						<div class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1">
 							{t('product.cta.details')}
@@ -55,15 +106,26 @@
 
 		<div class="justify-end w-full sm:w-1/3 sm:mt-0">
 			<a href="/product/{product._id}">
-				<PictureComponent picture={pictures[0]} class="h-[280px] mt-5 ml-auto object-contain" />
+				<PictureComponent
+					picture={pictures[pictureId]}
+					class="h-[280px] mt-5 ml-auto object-contain"
+				/>
 			</a>
 		</div>
 	</div>
 
 	<div class="ml-0 sm:ml-4 w-full sm:w-1/6 flex-col hidden sm:inline">
 		{#if pictures.length > 1}
-			{#each pictures as picture}
-				<PictureComponent {picture} class="w-[90px] h-[90px] rounded-sm mb-2 cursor-pointer" />
+			{#each pictures as picture, i}
+				<a href="#picture{i}" on:click={() => (pictureId = i)}>
+					<PictureComponent
+						{picture}
+						class="w-[100px] h-[100px] border-gray-300 border rounded object-cover mb-2 {pictureId ===
+						i
+							? 'ring-2 ring-link ring-offset-2'
+							: ''} "
+					/>
+				</a>
 			{/each}
 		{/if}
 	</div>
--- src/lib/components/ProductWidget/ProductWidgetVariation6.svelte
+++ src/lib/components/ProductWidget/ProductWidgetVariation6.svelte
@@ -3,6 +3,12 @@
 	import type { Picture } from '$lib/types/Picture';
 	import PictureComponent from '../Picture.svelte';
 	import { useI18n } from '$lib/i18n';
+	import Popup from '../Popup.svelte';
+	import { productAddedToCart } from '$lib/stores/productAddedToCart';
+	import ProductAddedToCart from '../ProductAddedToCart.svelte';
+	import { applyAction, enhance } from '$app/forms';
+	import { invalidate } from '$app/navigation';
+	import { UrlDependency } from '$lib/types/UrlDependency';
 
 	let className = '';
 	export { className as class };
@@ -20,21 +26,46 @@
 		| 'actionSettings'
 	>;
 	export let pictures: Picture[] | [];
+	export let canAddToCart: boolean;
+
+	const widget = {};
+	function addToCart() {
+		$productAddedToCart = {
+			product,
+			quantity: 1,
+			picture: pictures[0],
+			widget
+		};
+	}
+	let loading = false;
+
 	const { t } = useI18n();
+	let pictureId = 0;
 </script>
 
 <div class="flex flex-col mx-auto rounded p-4 sm:flex-row sm:gap-2 {className}">
 	<div class="ml-0 sm:ml-4 w-full sm:w-1/6 flex-col hidden sm:inline">
 		{#if pictures.length > 1}
-			{#each pictures as picture}
-				<PictureComponent {picture} class="w-[90px] h-[90px] rounded-sm mb-2 cursor-pointer" />
+			{#each pictures as picture, i}
+				<a href="#picture{i}" on:click={() => (pictureId = i)}>
+					<PictureComponent
+						{picture}
+						class="w-[100px] h-[100px] border-gray-300 border rounded object-cover mb-2 {pictureId ===
+						i
+							? 'ring-2 ring-link ring-offset-2'
+							: ''} "
+					/>
+				</a>
 			{/each}
 		{/if}
 	</div>
 	<div class="flex flex-wrap bg-gray-100 w-full sm:w-5/6 mb-4 sm:mb-0">
 		<div class="justify-center w-full sm:w-1/3 mt-4 sm:mt-0">
 			<a href="/product/{product._id}">
-				<PictureComponent picture={pictures[0]} class="h-[280px] mt-5 mr-auto object-contain" />
+				<PictureComponent
+					picture={pictures[pictureId]}
+					class="h-[280px] mt-5 mr-auto object-contain"
+				/>
 			</a>
 		</div>
 		<div class="p-4 w-full sm:w-2/3">
@@ -44,15 +75,49 @@
 			<a href="/product/{product._id}">
 				<p class="text-gray-600 mb-4">{product.shortDescription}</p>
 			</a>
-			<form method="post" class="contents">
+			<form
+				method="post"
+				class="contents"
+				use:enhance={() => {
+					loading = true;
+					return async ({ result }) => {
+						loading = false;
+						if (result.type === 'error') {
+							return await applyAction(result);
+						}
+
+						await invalidate(UrlDependency.Cart);
+						addToCart();
+						// Not for the widget, see https://github.com/B2Bitcoin/B2BitcoinBootik/issues/243
+						//document.body.scrollIntoView();
+					};
+				}}
+			>
 				<div class="flex flex-wrap gap-6 items-end">
-					<button
-						type="submit"
-						formaction="/product/{product._id}?/buy"
-						class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1"
-					>
-						{t('product.cta.buy')}
-					</button>
+					{#if canAddToCart}
+						<div class="relative">
+							<button
+								type="submit"
+								disabled={loading}
+								formaction="/product/{product._id}?/addToCart"
+								class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1"
+							>
+								{t('product.cta.buy')}
+							</button>
+
+							{#if $productAddedToCart && $productAddedToCart.widget === widget}
+								<Popup>
+									<ProductAddedToCart
+										class="w-[562px] max-w-full"
+										on:dismiss={() => ($productAddedToCart = null)}
+										product={$productAddedToCart.product}
+										picture={$productAddedToCart.picture}
+										customPrice={$productAddedToCart.customPrice}
+									/>
+								</Popup>
+							{/if}
+						</div>
+					{/if}
 					<a href="/product/{product._id}">
 						<div class="bg-blue-500 text-white text-xl text-center w-full md:w-[150px] p-1">
 							{t('product.cta.details')}
