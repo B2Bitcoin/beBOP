7e2648b60ddaffc89e0c2bafa78adb1f3ea19d04
unknown <ithiame@agiltoo.fr>
2023-11-17 08:49:58 +0000

    Theme & font manager generate css files

--- src/routes/theme.css/+server.ts
+++ src/routes/theme.css/+server.ts
@@ -4,6 +4,8 @@ import type { Style } from '$lib/types/Style';
 
 export const GET = async () => {
 	const theme = await collections.styles.findOne({ _id: runtimeConfig.mainThemeId });
+	console.log('hello' + theme);
+
 	const responseText = theme ? generateCss(theme, 'dark') : '';
 	return new Response(responseText, {
 		headers: {
@@ -33,9 +35,50 @@ function generateCss(themeData: Style, themeMode: string) {
 	processStyleObject(themeData);
 
 	return `
-	:root {
-	${css}
-  }
-  ${themeMode}
-  `;
+	@import 'tailwindcss/base';
+	@import 'tailwindcss/components';
+	@import 'tailwindcss/utilities';
+
+	:root {${css}}
+ 
+	body .secondPlan {
+		background-color: var(--body-secondPlan-backgroundColor-${themeMode});
+	  }
+	  
+	  body .mainPlan {
+		background-color: var(--body-mainPlan-backgroundColor-${themeMode});
+	  }
+	  
+	  body title {
+		font-family: var(--body-title-fontFamily);
+		color: var(--body-title-color-${themeMode});
+	  }
+	  
+	  body .text {
+		font-family: var(--body-text-fontFamily);
+	  }
+	  
+	  body .secondaryText {
+		font-family: var(--body-secondaryText-fontFamily);
+		color: var(--body-secondaryText-color-${themeMode});
+	  }
+	  
+	  body .mainCTA {
+		background-color: var(--body-mainCTA-backgroundColor-${themeMode});
+		color: var(--body-mainCTA-color-${themeMode});
+	  }
+	  
+	  body .secondaryCTA {
+		background-color: var(--body-secondaryCTA-backgroundColor-${themeMode});
+		color: var(--body-secondaryCTA-color-${themeMode});
+	  }
+	  
+	  body .cta {
+		font-family: var(--body-cta-fontFamily);
+	  }
+	  
+	  body .hyperlink {
+		color: var(--body-hyperlink-color-${themeMode});
+	  }
+ 	 `;
 }
