4520d949b259e77906cdc7cdbd3d62ba5221f22e
coyotte508 <coyotte508@gmail.com>
2023-11-18 17:56:07 +0100

    🐛 Add redirect after resetting password

--- src/routes/(app)/admin[[hash=admin_hash]]/login/reset/[token]/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/login/reset/[token]/+page.server.ts
@@ -1,11 +1,12 @@
 import { collections } from '$lib/server/database';
-import { error, fail } from '@sveltejs/kit';
+import { error, fail, redirect } from '@sveltejs/kit';
 import { z } from 'zod';
 import bcryptjs from 'bcryptjs';
 import { CUSTOMER_ROLE_ID, type User } from '$lib/types/User';
 import { ObjectId } from 'mongodb';
 import type { SetRequired } from 'type-fest';
 import { BCRYPT_SALT_ROUNDS } from '$lib/server/user.js';
+import { adminPrefix } from '$lib/server/admin.js';
 
 export async function load({ params }) {
 	const user = await collections.users.findOne<SetRequired<User, 'passwordReset'>>({
@@ -46,7 +47,7 @@ export const actions = {
 			{ $set: { password: passwordBcrypt }, $unset: { passwordReset: '' } }
 		);
 		if (updateResult.matchedCount) {
-			return { success: true };
+			throw redirect(303, `${adminPrefix()}/login`);
 		} else {
 			return fail(400, { failed: true });
 		}
