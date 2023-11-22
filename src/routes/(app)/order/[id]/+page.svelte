8c908beac9839a3b7e5eed54b251ef5bbe8af8d3
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2023-11-22 03:48:51 +0100

    Update +page.svelte

--- src/routes/(app)/order/[id]/+page.svelte
+++ src/routes/(app)/order/[id]/+page.svelte
@@ -36,9 +36,9 @@
 	const { t, locale } = useI18n();
 </script>
 
-<main class="mx-auto max-w-7xl py-10 px-6 body-secondPlan">
+<main class="mx-auto max-w-7xl py-10 px-6 body-mainPlan">
 	<div
-		class="w-full rounded-xl body-secondPlan border-gray-300 p-6 grid flex md:grid-cols-3 sm:flex-wrap gap-2"
+		class="w-full rounded-xl body-mainPlan border-gray-300 p-6 grid flex md:grid-cols-3 sm:flex-wrap gap-2"
 	>
 		<div class="col-span-2 flex flex-col gap-2">
 			<h1 class="text-3xl body-title">{t('order.singleTitle', { number: data.order.number })}</h1>
