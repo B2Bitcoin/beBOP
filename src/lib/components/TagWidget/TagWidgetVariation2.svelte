5bf6fee92c40b763511af7a4625cf531799572dc
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2023-11-22 05:59:00 +0100

    Update TagWidgetVariation2.svelte

--- src/lib/components/TagWidget/TagWidgetVariation2.svelte
+++ src/lib/components/TagWidget/TagWidgetVariation2.svelte
@@ -28,7 +28,7 @@
 			<div class="flex text-centern justify-evenly mt-auto">
 				{#each tag.cta as cta}
 					<div class="tagWidget-cta font-semibold text-xl text-center w-auto p-1">
-						<a class="tagWidget-hyperlink" href="{cta.href}">{cta.label}</a>
+						{cta.label}
 					</div>
 				{/each}
 			</div>
