7e339f5fbe8f7169cfad2ae435a55ea51792d8c8
coyotte508 <coyotte508@gmail.com>
2023-11-22 10:22:12 +0100

    🐛 Fix CMS full screen

--- src/routes/(app)/[slug]/+page.server.ts
+++ src/routes/(app)/[slug]/+page.server.ts
@@ -14,6 +14,7 @@ export async function load({ params, locals }) {
 
 	return {
 		cmsPage: omit(cmsPage, ['content']),
-		cmsData: cmsFromContent(cmsPage.content, locals.user?.roleId)
+		cmsData: cmsFromContent(cmsPage.content, locals.user?.roleId),
+		layoutReset: cmsPage.fullScreen
 	};
 }
