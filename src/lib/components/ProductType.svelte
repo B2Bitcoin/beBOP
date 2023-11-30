cf4eb839325cc4dde03b630260e2473e6f4b8ef5
unknown <ithiame@agiltoo.fr>
2023-11-30 18:07:03 +0000

    article type display on /order

--- src/lib/components/ProductType.svelte
+++ src/lib/components/ProductType.svelte
@@ -21,26 +21,28 @@
 </script>
 
 {#if isPreorder(product.availableDate, product.preorder)}
-	<span class="{baseClasses} {className} text-blue-500 bg-blue-200">
+	<span class="{baseClasses} {className} text-blue-500 bg-blue-200 whitespace-nowrap">
 		<IconDollar />
 		{t('product.type.preorder')}
 	</span>
 {/if}
 {#if !product.preorder && product.availableDate && product.availableDate > new Date()}
-	<span class="{baseClasses} {className} text-yellow-500 bg-yellow-100">
+	<span class="{baseClasses} {className} text-yellow-500 bg-yellow-100 whitespace-nowrap">
 		{t('product.type.preview')}
 	</span>
 {/if}
 
 {#if !(product.preorder && product.availableDate && product.availableDate > new Date()) && !product.shipping}
 	{#if product.type === 'resource' && !hasDigitalFiles}
-		<span class="{baseClasses} {className} text-roseofsharon-700 bg-roseofsharon-200">
+		<span
+			class="{baseClasses} {className} text-roseofsharon-700 bg-roseofsharon-200 whitespace-nowrap"
+		>
 			<IconDownTo />
 			{t('product.type.resource')}
 		</span>
 	{/if}
 	{#if product.type === 'resource' && hasDigitalFiles}
-		<span class="{baseClasses} {className} text-green-700 bg-green-200">
+		<span class="{baseClasses} {className} text-green-700 bg-green-200 whitespace-nowrap">
 			<IconDownTo />
 			{t('product.type.digitalResource')}
 		</span>
@@ -48,21 +50,25 @@
 {/if}
 
 {#if product.type === 'donation'}
-	<span class="{baseClasses} {className} text-rosebudcherry-700 bg-rosebudcherry-200">
+	<span
+		class="{baseClasses} {className} text-rosebudcherry-700 bg-rosebudcherry-200 whitespace-nowrap"
+	>
 		<IconHandHeart />
 		{t('product.type.donation')}
 	</span>
 {/if}
 
 {#if product.shipping}
-	<span class="{baseClasses} {className} text-roseofsharon-700 bg-roseofsharon-200">
+	<span
+		class="{baseClasses} {className} text-roseofsharon-700 bg-roseofsharon-200 whitespace-nowrap"
+	>
 		<IconBoxTaped />
 		{t('product.type.phisical')}
 	</span>
 {/if}
 
 {#if product.type === 'subscription'}
-	<span class="{baseClasses} {className} text-jagger-700 bg-jagger-200">
+	<span class="{baseClasses} {className} text-jagger-700 bg-jagger-200 whitespace-nowrap">
 		<IconRotate />
 		{t('product.type.subscription')}
 	</span>
