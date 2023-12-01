7b7b9ce1c805c2d36b5d77a875ce0fc983627209
coyotte508 <coyotte508@gmail.com>
2023-12-01 22:19:41 +0100

    💄 Add some spacing

--- src/routes/(app)/customer/orders/+page.svelte
+++ src/routes/(app)/customer/orders/+page.svelte
@@ -4,6 +4,8 @@
 	export let data;
 </script>
 
-<h1 class="text-3xl">List of your orders</h1>
+<main class="mx-auto max-w-7xl px-6 body-mainPlan flex flex-col gap-6">
+	<h1 class="text-3xl">List of your orders</h1>
 
-<OrdersList orders={data.orders} />
+	<OrdersList orders={data.orders} />
+</main>
