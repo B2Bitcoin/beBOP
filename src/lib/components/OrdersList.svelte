a32004eac34ed08fd358a7bd7665cf6edf473d92
coyotte508 <coyotte508@gmail.com>
2023-12-01 22:41:50 +0100

    🌐 i18n

--- src/lib/components/OrdersList.svelte
+++ src/lib/components/OrdersList.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { toSatoshis } from '$lib/utils/toSatoshis';
 	import { formatDistance } from 'date-fns';
 	import type { SimplifiedOrder } from '$lib/types/Order';
 	import PriceTag from './PriceTag.svelte';
 	import IconBitcoin from './icons/IconBitcoin.svelte';
 	import { currencies } from '$lib/stores/currencies';
+	import { useI18n } from '$lib/i18n';
 
 	export let orders:
 		| Pick<
@@ -13,12 +13,16 @@
 		  >[]
 		| [];
 	export let adminPrefix: string | undefined = undefined;
+
+	const { t, locale } = useI18n();
 </script>
 
 <ul class="flex flex-col gap-4">
 	{#each orders as order}
 		<li class="text-lg flex flex-wrap items-center gap-1">
-			<a href="/order/{order._id}" class="text-link hover:underline">#{order.number}</a>
+			<a href="/order/{order._id}" class="text-link hover:underline">
+				#{order.number.toLocaleString($locale)}
+			</a>
 			- {#if order.payment.method === 'bitcoin'}
 				<IconBitcoin />
 			{:else if order.payment.method === 'lightning'}
@@ -28,17 +32,15 @@
 			{:else if order.payment.method === 'card'}
 				💳
 			{/if} -
-			<time datetime={order.createdAt.toJSON()} title={order.createdAt.toLocaleString('en')}
-				>{formatDistance(order.createdAt, Date.now(), {
-					addSuffix: true
-				})}</time
+			<time datetime={order.createdAt.toJSON()} title={order.createdAt.toLocaleString($locale)}
+				>{order.createdAt.toLocaleDateString($locale)}</time
 			>
-			- Total: {toSatoshis(order.totalPrice.amount, order.totalPrice.currency).toLocaleString('en')}
-			SAT {#if $currencies.priceReference !== 'SAT'}(<PriceTag
+			- <PriceTag currency={order.totalPrice.currency} amount={order.totalPrice.amount} />
+			{#if adminPrefix}(<PriceTag
 					currency={order.totalPrice.currency}
 					amount={order.totalPrice.amount}
 					convertedTo={$currencies.priceReference}
-				/>){/if}-
+				/>){/if} -
 			<span
 				class={order.payment.status === 'expired' || order.payment.status === 'canceled'
 					? 'text-gray-550'
@@ -46,10 +48,11 @@
 					? 'text-green-500'
 					: ''}
 			>
-				{order.payment.status}</span
+				{t('order.paymentStatus.' + order.payment.status)}</span
 			>
-			{#if order.totalReceived}
-				- received: <PriceTag
+			{#if order.totalReceived && adminPrefix}
+				- {t('pos.order.satReceived')}
+				<PriceTag
 					class="inline-flex"
 					currency={order.totalReceived.currency}
 					amount={order.totalReceived.amount}
--- src/routes/(app)/customer/orders/+page.svelte
+++ src/routes/(app)/customer/orders/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import OrdersList from '$lib/components/OrdersList.svelte';
+	import { useI18n } from '$lib/i18n.js';
 
 	export let data;
+
+	const { t } = useI18n();
 </script>
 
 <main class="mx-auto max-w-7xl px-6 body-mainPlan flex flex-col gap-6">
-	<h1 class="text-3xl">List of your orders</h1>
+	<h1 class="text-3xl">{t('order.list.title')}</h1>
 
 	<OrdersList orders={data.orders} />
 </main>
