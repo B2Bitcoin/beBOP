564d40a32b41517270695edd855f32570815ee6f
ithiame <133223073+ithiame@users.noreply.github.com>
2023-12-04 09:57:45 +0000

    fix footer display (#707)

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -404,53 +404,41 @@
 		{/if}
 
 		<footer class="footer h-auto items-center flex">
-			<div class="mx-auto max-w-7xl px-6 py-6 flex items-start justify-evenly gap-2 w-full">
-				{#if data.displayCompanyInfo && data.sellerIdentity}
-					<!-- First column -->
-					<div>
-						<h3 class="text-lg font-semibold mb-2">{upperCase(t('company-identity'))}</h3>
-						{#if data.sellerIdentity.businessName}
-							<p>{data.sellerIdentity.businessName}</p>
-						{/if}
-						{#if data.sellerIdentity.address.state}
-							<p>{data.sellerIdentity.address.state}</p>
-						{/if}
-						<p>
-							{#if data.sellerIdentity.address.city}{data.sellerIdentity.address.city},
-							{/if}{#if data.sellerIdentity.address.street}{data.sellerIdentity.address.street},
-							{/if}{data.sellerIdentity.address.zip || ''}
-						</p>
-					</div>
-
-					<!-- Second column -->
-					{#if data.sellerIdentity.contact.email || data.sellerIdentity.contact.phone}
-						<div>
-							<h3 class="text-lg font-semibold mb-2">{upperCase(t('company-contact'))}</h3>
-							{#if data.sellerIdentity.contact.email}
-								<a href="mailto:{data.sellerIdentity.contact.email}">
-									{data.sellerIdentity.contact.email}
-								</a>
-							{/if}
-							{#if data.sellerIdentity.contact.phone}
-								<a href="tel:{data.sellerIdentity.contact.phone}">
-									{data.sellerIdentity.contact.phone}
-								</a>
-							{/if}
+			<div class="mx-auto max-w-7xl px-6 items-center gap-2 grow">
+				<div class="flex justify-between mb-4 mt-4">
+					{#if data.displayCompanyInfo}
+						<div class="flex w-3/5">
+							<!-- First column -->
+							<div class="w-1/2">
+								<h3 class="text-lg font-semibold mb-2">{upperCase(t('company-identity'))}</h3>
+								<p>{data.sellerIdentity?.businessName}</p>
+								<p>{data.sellerIdentity?.address.state}</p>
+								<p>
+									{data.sellerIdentity?.address.city}, {data.sellerIdentity?.address.street}, {data
+										.sellerIdentity?.address.zip}
+								</p>
+							</div>
+							<!-- Second column -->
+							<div class="w-1/2 pl-4">
+								<h3 class="text-lg font-semibold mb-2">{upperCase(t('company-contact'))}</h3>
+								<p>{data.sellerIdentity?.contact.email}</p>
+								<p>{data.sellerIdentity?.contact.phone}</p>
+							</div>
 						</div>
 					{/if}
-				{/if}
-
-				<div class="flex gap-4 items-center">
-					{#each data.links.footer as link}
-						<a href={link.href} data-sveltekit-preload-data="off">{link.label}</a>
-					{/each}
+					<div class="w-2/5">
+						<div class="ml-auto flex gap-4 items-center">
+							{#each data.links.footer as link}
+								<a href={link.href} data-sveltekit-preload-data="off">{link.label}</a>
+							{/each}
+						</div>
+					</div>
 				</div>
-
 				{#if data.displayPoweredBy}
 					<div class="flex justify-between mb-4 mt-4">
 						<a class="flex items-center gap-4" href="https://github.com/B2Bitcoin/beBOP"
 							><span class="font-light">{t('footer.poweredBy')} </span>
-							<img class="h-[40px] w-auto hidden dark:inline" src={DEFAULT_LOGO_DARK} alt="" />
+							<img class="h-[40px] w-auto hidden dark:inline" src={DEFAULT_LOGO} alt="" />
 							<img class="h-[40px] w-auto dark:hidden" src={DEFAULT_LOGO_DARK} alt="" />
 						</a>
 					</div>
