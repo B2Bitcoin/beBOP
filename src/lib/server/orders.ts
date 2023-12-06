17d361e750206f4b02e33059fa39bb8d38d7de6d
coyotte508 <coyotte508@gmail.com>
2023-12-06 12:43:44 +0100

    🐛 Clean old carts as well when creating an order

--- src/lib/server/orders.ts
+++ src/lib/server/orders.ts
@@ -710,7 +710,8 @@ export async function createOrder(
 		);
 
 		if (params.cart) {
-			await collections.carts.deleteOne({ _id: params.cart._id }, { session });
+			/** Also delete "old" carts with partial user info */
+			await collections.carts.deleteMany(userQuery(params.cart.user), { session });
 		}
 
 		for (const product of products) {
