c4a86fc1969b76d6ce37e90bcd338ed3bf1981f4
unknown <ithiame@agiltoo.fr>
2023-12-08 15:36:19 +0000

    fix billing digital product

--- src/routes/(app)/checkout/+page.svelte
+++ src/routes/(app)/checkout/+page.svelte
@@ -111,7 +111,7 @@
 		(discountType === 'fiat' &&
 			totalPriceWithVat > toSatoshis(discountAmount, data.currencies.main)) ||
 		(discountType === 'percentage' && discountAmount < 100);
-	let showBillingInfo = false;
+	$: showBillingInfo = false;
 </script>
 
 <main class="mx-auto max-w-7xl py-10 px-6 body-mainPlan">
@@ -129,91 +129,98 @@
 						{t('checkout.digitalNoShippingNeeded')}
 					</p>
 				{:else}
-					<label class="form-label col-span-3">
-						{t('address.firstName')}
-						<input
-							type="text"
-							class="form-input"
-							name="shipping.firstName"
-							autocomplete="given-name"
-							required
-							value={data.personalInfoConnected?.firstName ?? ''}
-						/>
-					</label>
+					{#if !isDigital}
+						<label class="form-label col-span-3">
+							{t('address.firstName')}
+							<input
+								type="text"
+								class="form-input"
+								name="shipping.firstName"
+								autocomplete="given-name"
+								required
+								value={data.personalInfoConnected?.firstName ?? ''}
+							/>
+						</label>
 
-					<label class="form-label col-span-3">
-						{t('address.lastName')}
-						<input
-							type="text"
-							class="form-input"
-							name="shipping.lastName"
-							autocomplete="family-name"
-							required
-							value={data.personalInfoConnected?.lastName ?? ''}
-						/>
-					</label>
+						<label class="form-label col-span-3">
+							{t('address.lastName')}
+							<input
+								type="text"
+								class="form-input"
+								name="shipping.lastName"
+								autocomplete="family-name"
+								required
+								value={data.personalInfoConnected?.lastName ?? ''}
+							/>
+						</label>
 
-					<label class="form-label col-span-6">
-						{t('address.address')}
-						<input
-							type="text"
-							class="form-input"
-							autocomplete="street-address"
-							name="shipping.address"
-							required
-							value={data.personalInfoConnected?.address?.street ?? ''}
-						/>
-					</label>
+						<label class="form-label col-span-6">
+							{t('address.address')}
+							<input
+								type="text"
+								class="form-input"
+								autocomplete="street-address"
+								name="shipping.address"
+								required
+								value={data.personalInfoConnected?.address?.street ?? ''}
+							/>
+						</label>
 
-					<label class="form-label col-span-3">
-						{t('address.country')}
-						<select name="country" class="form-input" required bind:value={country}>
-							{#each sortedCountryCodes() as code}
-								<option
-									value={code}
-									selected={code === data.personalInfoConnected?.address?.country}
-									>{countryName(code)}</option
-								>
-							{/each}
-						</select>
-					</label>
+						<label class="form-label col-span-3">
+							{t('address.country')}
+							<select name="country" class="form-input" required bind:value={country}>
+								{#each sortedCountryCodes() as code}
+									<option
+										value={code}
+										selected={code === data.personalInfoConnected?.address?.country}
+										>{countryName(code)}</option
+									>
+								{/each}
+							</select>
+						</label>
 
-					<span class="col-span-3" />
+						<span class="col-span-3" />
 
-					<label class="form-label col-span-2">
-						{t('address.state')}
+						<label class="form-label col-span-2">
+							{t('address.state')}
 
-						<input
-							type="text"
-							name="shipping.state"
-							class="form-input"
-							value={data.personalInfoConnected?.address?.state ?? ''}
-						/>
-					</label>
-					<label class="form-label col-span-2">
-						{t('address.city')}
+							<input
+								type="text"
+								name="shipping.state"
+								class="form-input"
+								value={data.personalInfoConnected?.address?.state ?? ''}
+							/>
+						</label>
+						<label class="form-label col-span-2">
+							{t('address.city')}
 
-						<input
-							type="text"
-							name="shipping.city"
-							class="form-input"
-							value={data.personalInfoConnected?.address?.city ?? ''}
-							required
-						/>
-					</label>
-					<label class="form-label col-span-2">
-						{t('address.zipCode')}
+							<input
+								type="text"
+								name="shipping.city"
+								class="form-input"
+								value={data.personalInfoConnected?.address?.city ?? ''}
+								required
+							/>
+						</label>
+						<label class="form-label col-span-2">
+							{t('address.zipCode')}
 
-						<input
-							type="text"
-							name="shipping.zip"
-							class="form-input"
-							value={data.personalInfoConnected?.address?.zip ?? ''}
-							required
-							autocomplete="postal-code"
-						/>
-					</label>
-					{#if data.isBillingAddressMandatory}
+							<input
+								type="text"
+								name="shipping.zip"
+								class="form-input"
+								value={data.personalInfoConnected?.address?.zip ?? ''}
+								required
+								autocomplete="postal-code"
+							/>
+						</label>
+					{:else}
+						<p class="col-span-6">
+							{t('checkout.digitalNoShippingNeeded')}
+						</p>
+					{/if}
+
+					{#if !isDigital && data.isBillingAddressMandatory}
 						<label class="col-span-6 checkbox-label">
 							<input
 								type="checkbox"
@@ -228,7 +235,7 @@
 				{/if}
 			</section>
 
-			{#if showBillingInfo}
+			{#if showBillingInfo || (isDigital && data.collectBillingAddressOnDeliverylessOrders)}
 				<section class="gap-4 grid grid-cols-6 w-4/5">
 					<h2 class="font-light text-2xl col-span-6">{t('checkout.billingInfo')}</h2>
 
