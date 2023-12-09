0df5d6c4426066bf0c56d9d93679ff5d41d4a779
unknown <ithiame@agiltoo.fr>
2023-12-09 17:05:29 +0000

    specification widget fix

--- src/lib/utils/specification.ts
+++ src/lib/utils/specification.ts
@@ -1,3 +1,6 @@
+import { trimPrefix } from './trimPrefix';
+import { trimSuffix } from './trimSuffix';
+
 interface CategorySpecification {
 	[key: string]: { attribute: string; value: string }[];
 }
@@ -5,7 +8,9 @@ interface CategorySpecification {
 export function specificationGroupBy(content: string): CategorySpecification {
 	const groupedSpecification: CategorySpecification = {};
 	for (const line of content.split('\n')) {
-		const [category, attribute, value] = line.split(';').map((entry) => entry.replace(/"/g, ''));
+		const [category, attribute, value] = line
+			.split(';')
+			.map((entry) => trimPrefix(trimSuffix(entry, '"'), '"'));
 
 		if (!groupedSpecification[category]) {
 			groupedSpecification[category] = [];
