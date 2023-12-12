cb0c1675b9c1309cb60f99c09e2fb86ad8cf8f1d
coyotte508 <coyotte508@gmail.com>
2023-12-13 00:42:18 +0100

    ✅ Add zpub test for testnet

--- src/lib/server/bitcoin.test.ts
+++ src/lib/server/bitcoin.test.ts
@@ -13,4 +13,16 @@ describe('bitcoin', () => {
 		expect(bip84Address(zpub, 4)).toBe('bc1qldvg4ldzcy8v00gvy7m22uv68fw7gz72ky7v6l');
 		expect(bip84Address(zpub, 5)).toBe('bc1qlgxhugactwcmz57hsreydlpvldx4ud5aj8fhh4');
 	});
+
+	it('should derivate public key for testnet', () => {
+		const vpub =
+			'vpub5YoHT14yexuoFrVBLXNbAUhniupoaUZXin3EXfJCpg2WhdrYiNSDW7CsFHyh3JoG26MnDv3JgDWZXjauyXdo9S46E2xZXdzgi9SXEiPyBUY';
+
+		expect(bip84Address(vpub, 0)).toBe('tb1qrw2swpufzdx9gy4aewv5q45e53stcf95ulcu6d');
+		expect(bip84Address(vpub, 1)).toBe('tb1qa07qeugkc8u7pjv98dejfdnd5ml7qxv8dy43cq');
+		expect(bip84Address(vpub, 2)).toBe('tb1qlyaf3z5auqt0da07303xhhehqzxx797dm7pdys');
+		expect(bip84Address(vpub, 3)).toBe('tb1qsh4z5ntmcva4a0aj5u2u9c6xsx4rdr4jycqe76');
+		expect(bip84Address(vpub, 4)).toBe('tb1qldvg4ldzcy8v00gvy7m22uv68fw7gz72uz9lpv');
+		expect(bip84Address(vpub, 5)).toBe('tb1qlgxhugactwcmz57hsreydlpvldx4ud5acpjyvx');
+	});
 });
