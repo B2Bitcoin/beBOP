af91bb825afb94329e2e8c40fc298c35cf9d90c3
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2023-12-14 10:51:35 +0100

    Specification widget translation key (#819)
    Co-authored-by: Eliott C. <coyotte508@gmail.com>

--- src/lib/components/SpecificationWidget.svelte
+++ src/lib/components/SpecificationWidget.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import type { Specification } from '$lib/types/Specification';
 	import { specificationGroupBy } from '$lib/utils/specification';
+	import { useI18n } from '$lib/i18n';
 
 	export let specification: Pick<Specification, 'title' | 'content'>;
 
 	let className = '';
 	export { className as class };
 	let specificationCategory = specificationGroupBy(specification.content);
+	const { t } = useI18n();
 </script>
 
 <div class="relative mx-auto tagWidget tagWidget-main p-6 rounded {className}">
-	<h2 class="text-2xl body-title">Specification</h2>
+	<h2 class="text-2xl body-title">{t('widget.specification')}</h2>
 	<div class="flex flex-row gap-6">
 		{#each Object.keys(specificationCategory) as category (category)}
 			<div class="flex-col">
