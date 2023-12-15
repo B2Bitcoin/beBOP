fee111c8844e8973f5f6f874779f8ea5e4fd2ab5
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2023-12-15 14:27:10 +0100

    Make reverse variation of tag widget 4 #846 (#845)

--- src/lib/components/TagWidget/TagWidgetVariation4Reverse.svelte
+++ src/lib/components/TagWidget/TagWidgetVariation4Reverse.svelte
@@ -14,6 +14,14 @@
 </script>
 
 <div class="mx-auto tagWidget tagWidget-main flex rounded sm:gap-2 {className}">
+
+	<div class="relative">
+		<PictureComponent {picture} />
+		<div class="tagWidget-cta font-semibold text-xl absolute bottom-0 right-0 text-center p-1 m-2">
+			<a class="tagWidget-hyperlink" href={tag.cta[0].href}>{tag.cta[0].label}</a>
+		</div>
+	</div>
+
 	<div class="flex flex-col ml-2">
 		<h2 class="text-4xl pb-2 uppercase body-title">{tag.title}</h2>
 		<h2 class="text-lg pb-2 md:text-3xl">
@@ -21,10 +29,4 @@
 		</h2>
 	</div>
 
-	<div class="relative">
-		<PictureComponent {picture} />
-		<div class="tagWidget-cta font-semibold text-xl absolute bottom-0 left-0 text-center p-1 m-2">
-			<a class="tagWidget-hyperlink" href={tag.cta[0].href}>{tag.cta[0].label}</a>
-		</div>
-	</div>
 </div>
