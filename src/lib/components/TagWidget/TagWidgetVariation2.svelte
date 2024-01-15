df14845d26d697e1178bd1a8a4c21675c0d1535f
unknown <ithiame@agiltoo.fr>
2024-01-15 11:24:18 +0000

    Add rounded corner style on every widget CTA, like btn class, and harmonize style

--- src/lib/components/TagWidget/TagWidgetVariation2.svelte
+++ src/lib/components/TagWidget/TagWidgetVariation2.svelte
@@ -27,7 +27,7 @@
 			</h2>
 			<div class="flex text-centern justify-evenly mt-auto mb-2">
 				{#each tag.cta as cta}
-					<div class="tagWidget-cta font-semibold text-xl text-center w-auto p-1">
+					<div class="btn tagWidget-cta text-xl text-center w-auto p-3">
 						<a class="tagWidget-hyperlink" href={cta.href}>{cta.label}</a>
 					</div>
 				{/each}
