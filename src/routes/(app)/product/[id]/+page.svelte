690f1d201f5c9189e18658497aed9846f84db796
unknown <ithiame@agiltoo.fr>
2024-01-17 14:29:59 +0000

    CTA custom display

--- src/routes/(app)/product/[id]/+page.svelte
+++ src/routes/(app)/product/[id]/+page.svelte
@@ -442,7 +442,7 @@
 							<a href={cta.href} class="btn body-cta body-secondaryCTA">
 								{cta.label}
 							</a>
-						{:else if !canBuy && amountAvailable < 0}
+						{:else if !canBuy && amountAvailable > 0}
 							<a href={cta.href} class="btn body-cta body-secondaryCTA">
 								{cta.label}
 							</a>
