82c92be9488a731120c5d00b167e280d8070f695
unknown <ithiame@agiltoo.fr>
2024-01-19 09:08:35 +0000

    Create countdown widget #915

--- src/routes/(app)/countdown/[id]/+page.svelte
+++ src/routes/(app)/countdown/[id]/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import CountdownWidget from '$lib/components/CountdownWidget.svelte';
-	import { useI18n } from '$lib/i18n';
-
-	const { t } = useI18n();
 
 	export let data;
 </script>
