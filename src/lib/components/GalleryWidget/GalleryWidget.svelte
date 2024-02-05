c205830c929f4b2e5bce5931e4fdc3592dbddb72
unknown <ithiame@agiltoo.fr>
2024-02-05 14:26:43 +0000

    gallery picture ration

--- src/lib/components/GalleryWidget/GalleryWidget.svelte
+++ src/lib/components/GalleryWidget/GalleryWidget.svelte
@@ -19,38 +19,38 @@
 		: ''}"
 >
 	<div class="flex-col">
-		<div class="tagWidget tagWidget-main m-2 p-4 max-w-[344px]">
+		<div class="tagWidget tagWidget-main m-2 p-4">
 			<h2 class="text-2xl body-title pb-2 uppercase">{gallery.principal.title}</h2>
 		</div>
-		<div class="btn tagWidget-cta text-xl text-center w-auto m-2 p-4 max-w-[344px]">
+		<div class="btn tagWidget-cta text-xl text-center w-auto m-2 p-4">
 			<a class="tagWidget-hyperlink" href={gallery.principal.cta.href}
 				>{gallery.principal.cta.label}</a
 			>
 		</div>
-		<div class="tagWidget tagWidget-main m-2 p-4 max-w-[344px]">
-			<p class="min-h-[630px] mt-2">
+		<div class="tagWidget tagWidget-main m-2 p-4">
+			<p class="min-h-[585px] mt-2">
 				{gallery.principal.content}
 			</p>
 		</div>
 	</div>
 	{#each gallery.secondary as secondary}
 		<div class="flex-col">
-			<div class="m-2 justify-items-center max-w-[344px]">
+			<div class="m-2 justify-items-center">
 				<PictureComponent
 					picture={pictureById[secondary.pictureId || '']}
-					class="block h-auto w-[344px]"
+					class="block h-auto w-auto"
 				/>
 			</div>
 
-			<div class="m-2 tagWidget tagWidget-main p-4 text-center max-w-[344px]">
+			<div class="m-2 tagWidget tagWidget-main p-4 text-center">
 				<h2 class="text-xl body-title pb-2 uppercase">{secondary.title}</h2>
 			</div>
-			<div class="m-2 tagWidget tagWidget-main p-4 text-center max-w-[344px]">
+			<div class="m-2 tagWidget tagWidget-main p-4 text-center">
 				<p class="min-h-[285px] mt-2">
 					{secondary.content}
 				</p>
 			</div>
-			<div class="btn tagWidget-cta text-xl text-center w-auto p-4 m-2 max-w-[344px]">
+			<div class="btn tagWidget-cta text-xl text-center w-auto p-4 m-2">
 				<a class="tagWidget-hyperlink" href={secondary.cta.href}>{secondary.cta.label}</a>
 			</div>
 		</div>
