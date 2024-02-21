204f9eae9df06b6b419cc292609a5ef5695dc098
ithiame <133223073+ithiame@users.noreply.github.com>
2024-02-21 09:03:38 +0000

    Create basic sales extract for owners #161 (#985)
    Co-authored-by: coyotte508 <coyotte508@gmail.com>

--- src/lib/server/pojo.ts
+++ src/lib/server/pojo.ts
@@ -1,17 +1,16 @@
 import { ObjectId } from 'mongodb';
 
 export type Pojo<T> = {
-	[K in keyof T]: T[K] extends ObjectId
-		? string
-		: T[K] extends ObjectId | undefined
-		? string | undefined
-		: T[K];
+	[K in keyof T]: ObjectId extends T[K] ? Exclude<T[K], ObjectId> | string : T[K];
 };
 
 /**
  * Convert an object into a POJO by converting ObjectId to string
  */
-export function pojo<T extends Record<string, unknown>>(param: T): Pojo<T> {
+export function pojo<T extends object>(param: T): Pojo<T> {
+	if (Array.isArray(param)) {
+		throw new Error('pojo does not support arrays yet');
+	}
 	return Object.fromEntries(
 		Object.entries(param).map(([key, value]) => {
 			if (value instanceof ObjectId) {
