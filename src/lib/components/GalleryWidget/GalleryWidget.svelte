10410d65345e1cb436730f1c4d1c47d73c1ff240
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2024-02-24 11:46:13 +0100

    Update GalleryWidget.svelte
    Will need a proper configuration in gallery admin later (checkbox like sliders)

--- src/lib/components/GalleryWidget/GalleryWidget.svelte
+++ src/lib/components/GalleryWidget/GalleryWidget.svelte
@@ -51,7 +51,7 @@
 				</p>
 			</div>
 			<div class="btn tagWidget-cta text-xl text-center w-auto p-4 m-2">
-				<a class="tagWidget-hyperlink" href={secondary.cta.href}>{secondary.cta.label}</a>
+				<a class="tagWidget-hyperlink" target="_blank" href={secondary.cta.href}>{secondary.cta.label}</a>
 			</div>
 		</div>
 	{/each}
