7605091f30b249c3b1f1e17a95253d5e57baedb1
coyotte508 <coyotte508@gmail.com>
2024-03-04 18:37:28 +0100

    ✨ Recursive pojo

--- src/lib/server/pojo.ts
+++ src/lib/server/pojo.ts
@@ -1,15 +1,42 @@
 import { ObjectId } from 'mongodb';
+import type { IsUnknown, UnknownArray } from 'type-fest';
 
-export type Pojo<T> = {
-	[K in keyof T]: ObjectId extends T[K] ? Exclude<T[K], ObjectId> | string : T[K];
+export type PojoList<T extends UnknownArray> = T extends readonly []
+	? []
+	: T extends readonly [infer F, ...infer R]
+	? [Pojo<F>, ...PojoList<R>]
+	: IsUnknown<T[number]> extends true
+	? []
+	: Array<Pojo<T[number]>>;
+
+export type PojoObject<T extends object> = {
+	[K in keyof T]: ObjectId extends T[K] ? Pojo<Exclude<T[K], ObjectId>> | string : Pojo<T[K]>;
 };
+
+export type Pojo<T> = ObjectId extends T
+	? Exclude<Pojo<T>, ObjectId> | string
+	: T extends UnknownArray
+	? PojoList<T>
+	: T extends object
+	? PojoObject<T>
+	: T;
 
 /**
  * Convert an object into a POJO by converting ObjectId to string
  */
-export function pojo<T extends object>(param: T): Pojo<T> {
+export function pojo<T>(param: T): Pojo<T> {
+	if (param === null || typeof param !== 'object') {
+		return param as Pojo<T>;
+	}
+	if (param instanceof ObjectId) {
+		return param.toHexString() as Pojo<T>;
+	}
 	if (Array.isArray(param)) {
-		throw new Error('pojo does not support arrays yet');
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return param.map((x) => (pojo as any)(x)) as Pojo<T>;
+	}
+	if (Object.getPrototypeOf(param) !== Object.prototype) {
+		return param as Pojo<T>;
 	}
 	return Object.fromEntries(
 		Object.entries(param).map(([key, value]) => {
