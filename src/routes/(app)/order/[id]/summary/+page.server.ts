b254ef8c7cfea6c9de48d46c63acff6113afded3
unknown <ithiame@agiltoo.fr>
2024-03-13 16:12:33 +0000

    Create a synthetis document for multi-payment orders #1133

--- src/routes/(app)/order/[id]/summary/+page.server.ts
+++ src/routes/(app)/order/[id]/summary/+page.server.ts
@@ -1,9 +1,16 @@
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import { runtimeConfig } from '$lib/server/runtime-config.js';
 import { fetchOrderForUser } from '../fetchOrderForUser';
+import { CUSTOMER_ROLE_ID } from '$lib/types/User';
 
-export async function load({ params }) {
+export async function load({ params, locals }) {
 	const order = await fetchOrderForUser(params.id);
+	if (!locals.user) {
+		throw redirect(303, '/admin/login');
+	}
+	if (locals.user.roleId === CUSTOMER_ROLE_ID) {
+		throw error(403, 'You are not allowed to access this page, only employee accounts are.');
+	}
 	if (!order) {
 		throw error(404, 'Order not found');
 	}
