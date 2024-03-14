ef9dba68f1c1d22a7fb32323bcf44e4d6e370838
unknown <ithiame@agiltoo.fr>
2024-03-14 08:25:32 +0000

    Display a warning / confirm message when cancelling a pending payment (employee / POS seat) #1145

--- src/routes/(app)/order/[id]/+page.svelte
+++ src/routes/(app)/order/[id]/+page.svelte
@@ -50,6 +50,11 @@
 	);
 
 	$: remainingAmount = orderAmountWithNoPaymentsCreated(data.order);
+	function confirmCancel(event: Event) {
+		if (!confirm(t('order.confirmCancel'))) {
+			event.preventDefault();
+		}
+	}
 </script>
 
 <main class="mx-auto max-w-7xl py-10 px-6 body-mainPlan">
@@ -258,10 +263,7 @@
 										method="post"
 										class="contents"
 									>
-										<button
-											type="submit"
-											class="btn btn-red"
-											on:click={() => confirm(t('order.confirmCancel'))}
+										<button type="submit" class="btn btn-red" on:click={confirmCancel}
 											>{t('pos.cta.cancelOrder')}</button
 										>
 									</form>
