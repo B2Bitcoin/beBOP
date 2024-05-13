db9524ba26fb732c86b2ee0f9884e96a35c0f0d6
unknown <ithiame@agiltoo.fr>
2024-05-13 14:19:01 +0000

    Countdown suggestion

--- src/lib/components/CountdownWidget.svelte
+++ src/lib/components/CountdownWidget.svelte
@@ -23,28 +23,11 @@
 		<h2 class="text-2xl font-bold mb-2 body-title">
 			{countdown.title}
 		</h2>
-		<div class="grow-[2] lg:hidden flex flex-row gap-4 p-4 justify-end">
-			{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
-				<div class="flex flex-col">
-					<span class="text-3xl font-bold body-title">
-						{Math.floor(
-							unit === 'DAYS'
-								? distance / (1000 * 60 * 60 * 24)
-								: unit === 'HOURS'
-								? (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-								: unit === 'MINS'
-								? (distance % (1000 * 60 * 60)) / (1000 * 60)
-								: (distance % (1000 * 60)) / 1000
-						)}
-					</span>
-					{unit}
-				</div>
-			{/each}
-		</div>
+
 		<p class="mb-4">{countdown.description}</p>
 	</div>
 
-	<div class="grow-[2] flex-row gap-6 p-4 hidden lg:flex">
+	<div class="grow-[2] flex flex-row gap-4 p-4 lg:justify-normal justify-end">
 		{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
 			<div class="flex flex-col">
 				<span class="text-3xl font-bold body-title">
