071c6ba0e7aa86c649060b148804dd79283ebcf5
unknown <ithiame@agiltoo.fr>
2024-05-13 14:14:39 +0000

    Countdown suggestion

--- src/lib/components/CountdownWidget.svelte
+++ src/lib/components/CountdownWidget.svelte
@@ -18,17 +18,12 @@
 	});
 </script>
 
-<div class="hidden sm:contents gap-4 {className}">
-	<div class="flex flex-row w-full tagWidget tagWidget-main grow pr-5">
-		<div class="p-4 grow">
-			<h2 class="text-2xl font-bold mb-2 body-title">
-				{countdown.title}
-			</h2>
-
-			<p class="mb-4">{countdown.description}</p>
-		</div>
-
-		<div class="grow-[2] flex flex-row gap-6 p-4">
+<div class="flex lg:flex-row flex-col w-full tagWidget tagWidget-main grow pr-5">
+	<div class="p-4 grow">
+		<h2 class="text-2xl font-bold mb-2 body-title">
+			{countdown.title}
+		</h2>
+		<div class="grow-[2] lg:hidden flex flex-row gap-4 p-4 justify-end">
 			{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
 				<div class="flex flex-col">
 					<span class="text-3xl font-bold body-title">
@@ -46,35 +41,25 @@
 				</div>
 			{/each}
 		</div>
+		<p class="mb-4">{countdown.description}</p>
 	</div>
-</div>
 
-<div class="sm:hidden contents gap-4 {className}">
-	<div class="flex flex-col w-full tagWidget tagWidget-main grow pr-5">
-		<div class="p-4 grow">
-			<h2 class="text-2xl font-bold mb-2 body-title">
-				{countdown.title}
-			</h2>
-
-			<div class="grow-[2] flex flex-row gap-6 p-4 justify-end">
-				{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
-					<div class="flex flex-col">
-						<span class="text-3xl font-bold body-title">
-							{Math.floor(
-								unit === 'DAYS'
-									? distance / (1000 * 60 * 60 * 24)
-									: unit === 'HOURS'
-									? (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-									: unit === 'MINS'
-									? (distance % (1000 * 60 * 60)) / (1000 * 60)
-									: (distance % (1000 * 60)) / 1000
-							)}
-						</span>
-						{unit}
-					</div>
-				{/each}
+	<div class="grow-[2] flex-row gap-6 p-4 hidden lg:flex">
+		{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
+			<div class="flex flex-col">
+				<span class="text-3xl font-bold body-title">
+					{Math.floor(
+						unit === 'DAYS'
+							? distance / (1000 * 60 * 60 * 24)
+							: unit === 'HOURS'
+							? (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+							: unit === 'MINS'
+							? (distance % (1000 * 60 * 60)) / (1000 * 60)
+							: (distance % (1000 * 60)) / 1000
+					)}
+				</span>
+				{unit}
 			</div>
-			<p class="mb-4">{countdown.description}</p>
-		</div>
+		{/each}
 	</div>
 </div>
