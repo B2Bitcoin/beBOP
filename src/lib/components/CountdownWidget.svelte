2a9cb5fdb2253a4236e4f1310b5196ebcc152348
unknown <ithiame@agiltoo.fr>
2024-05-14 09:55:32 +0000

    Countdown suggestion

--- src/lib/components/CountdownWidget.svelte
+++ src/lib/components/CountdownWidget.svelte
@@ -18,31 +18,33 @@
 	});
 </script>
 
-<div class="flex lg:flex-row flex-col w-full tagWidget tagWidget-main grow pr-5 {className}">
-	<div class="p-4 grow">
-		<h2 class="text-2xl font-bold mb-2 body-title">
-			{countdown.title}
-		</h2>
+<div class="flex flex-row gap-4 {className}">
+	<div class="flex lg:flex-row flex-col w-full tagWidget tagWidget-main grow pr-5">
+		<div class="p-4 grow">
+			<h2 class="text-2xl font-bold mb-2 body-title">
+				{countdown.title}
+			</h2>
 
-		<p class="mb-4">{countdown.description}</p>
-	</div>
+			<p class="mb-4">{countdown.description}</p>
+		</div>
 
-	<div class="grow-[2] flex flex-row gap-4 p-4 lg:justify-normal justify-end">
-		{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
-			<div class="flex flex-col">
-				<span class="text-3xl font-bold body-title">
-					{Math.floor(
-						unit === 'DAYS'
-							? distance / (1000 * 60 * 60 * 24)
-							: unit === 'HOURS'
-							? (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-							: unit === 'MINS'
-							? (distance % (1000 * 60 * 60)) / (1000 * 60)
-							: (distance % (1000 * 60)) / 1000
-					)}
-				</span>
-				{unit}
-			</div>
-		{/each}
+		<div class="grow-[2] flex flex-row gap-4 p-4 lg:justify-normal justify-end">
+			{#each ['DAYS', 'HOURS', 'MINS', 'SECS'] as unit (unit)}
+				<div class="flex flex-col">
+					<span class="text-3xl font-bold body-title">
+						{Math.floor(
+							unit === 'DAYS'
+								? distance / (1000 * 60 * 60 * 24)
+								: unit === 'HOURS'
+								? (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+								: unit === 'MINS'
+								? (distance % (1000 * 60 * 60)) / (1000 * 60)
+								: (distance % (1000 * 60)) / 1000
+						)}
+					</span>
+					{unit}
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
