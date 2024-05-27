333b7e1deb73faaca5121ef98cedd5e2a83628bf
unknown <ithiame@agiltoo.fr>
2024-05-27 16:41:12 +0000

    use tiny-slider in slide

--- src/lib/components/CarouselWidget.svelte
+++ src/lib/components/CarouselWidget.svelte
@@ -2,18 +2,18 @@
 	import type { Picture } from '$lib/types/Picture';
 	import { TinySlider } from 'svelte-tiny-slider';
 	import PictureComponent from './Picture.svelte';
+	import { fade } from 'svelte/transition';
 
 	export let pictures: Picture[];
 	export let autoplay: number;
 	let className = '';
 	export { className as class };
-	const transitionDuration = 3;
 </script>
 
 <div class={className}>
-	<TinySlider let:currentIndex transitionDuration>
+	<TinySlider let:currentIndex>
 		{#each pictures as picture, i}
-			<div class="flex-row">
+			<div class="flex-row" transition:fade={{ duration: autoplay }}>
 				{#if currentIndex === i}
 					<a href={picture.slider?.url} target={picture.slider?.openNewTab ? '_blank' : '_self'}>
 						<PictureComponent {picture} class="object-fill h-auto w-auto " /></a
