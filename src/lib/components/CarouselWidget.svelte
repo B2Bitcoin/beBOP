30fc3cf8a850caefc5d8e008131f03aee12b4283
unknown <ithiame@agiltoo.fr>
2024-05-28 09:53:38 +0000

    use tiny-slider in slide add controls and autoplay delete flowbite

--- src/lib/components/CarouselWidget.svelte
+++ src/lib/components/CarouselWidget.svelte
@@ -2,18 +2,35 @@
 	import type { Picture } from '$lib/types/Picture';
 	import { TinySlider } from 'svelte-tiny-slider';
 	import PictureComponent from './Picture.svelte';
-	import { fade } from 'svelte/transition';
+	import { onDestroy, onMount } from 'svelte';
 
 	export let pictures: Picture[];
 	export let autoplay: number;
 	let className = '';
 	export { className as class };
+	let currentIndex = 0;
+	// Function to move to the next slide
+	function nextSlide() {
+		currentIndex = (currentIndex + 1) % pictures.length;
+	}
+	const interval = setInterval(nextSlide, autoplay);
+	onDestroy(() => {
+		clearInterval(interval);
+	});
+	onMount(() => {
+		return () => clearInterval(interval);
+	});
+	function setIndex(index: number) {
+		currentIndex = index;
+		clearInterval(interval);
+		setInterval(nextSlide, autoplay);
+	}
 </script>
 
 <div class={className}>
-	<TinySlider let:currentIndex>
+	<TinySlider {currentIndex} transitionDuration={200}>
 		{#each pictures as picture, i}
-			<div class="flex-row" transition:fade={{ duration: autoplay }}>
+			<div class="flex-row">
 				{#if currentIndex === i}
 					<a href={picture.slider?.url} target={picture.slider?.openNewTab ? '_blank' : '_self'}>
 						<PictureComponent {picture} class="object-fill h-auto w-auto " /></a
@@ -21,5 +38,18 @@
 				{/if}
 			</div>
 		{/each}
+		<svelte:fragment slot="controls" let:currentIndex>
+			<!-- Pagination Points -->
+			<div class="relative bottom-10 mx-auto w-full flex justify-center p-4 space-x-2">
+				{#each pictures as _, i}
+					<button
+						class="w-3 h-3 rounded-full transition-colors duration-300"
+						class:body-mainCTA={currentIndex === i}
+						class:bg-gray-200={currentIndex !== i}
+						on:click={() => setIndex(i)}
+					/>
+				{/each}
+			</div>
+		</svelte:fragment>
 	</TinySlider>
 </div>
