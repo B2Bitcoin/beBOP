8efa82e8ec858066e236ee731d80aa2292275a7c
unknown <ithiame@agiltoo.fr>
2024-05-29 13:11:57 +0000

    Allow Superadmin (and only Superadmin) account to edit its contact information on /admin/arm #1185

--- src/routes/(app)/admin[[hash=admin_hash]]/arm/user/[id]/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/arm/user/[id]/+page.server.ts
@@ -48,36 +48,25 @@ export const actions = {
 		if (user.roleId !== SUPER_ADMIN_ROLE_ID && !parsed.recoveryEmail && !parsed.recoveryNpub) {
 			throw error(400, 'You must provide a recovery email or npub');
 		}
-		if (user.roleId === SUPER_ADMIN_ROLE_ID) {
-			await collections.users.updateOne(
-				{ _id: user._id },
-				{
-					$set: {
-						...(parsedAdmin.alias && { alias: parsedAdmin.alias })
-					}
-				}
-			);
 
-			throw redirect(303, `${adminPrefix()}/arm`);
-		} else {
-			await collections.users.updateOne(
-				{ _id: user._id },
-				{
-					$set: {
-						login: parsed.login,
-						...(parsed.alias && { alias: parsed.alias }),
-						recovery: {
-							...(parsed.recoveryEmail && { email: parsed.recoveryEmail }),
-							...(parsed.recoveryNpub && { npub: parsed.recoveryNpub })
-						},
-						disabled: parsed.status === 'disabled',
-						roleId: parsed.roleId
-					}
+		await collections.users.updateOne(
+			{ _id: user._id },
+			{
+				$set: {
+					login: parsed.login ?? user.login,
+					...(parsed.alias && { alias: parsed.alias }),
+					recovery: {
+						...(parsed.recoveryEmail && { email: parsed.recoveryEmail }),
+						...(parsed.recoveryNpub && { npub: parsed.recoveryNpub })
+					},
+					...(parsedAdmin.alias && { alias: parsedAdmin.alias }),
+					disabled: parsed.status === 'disabled',
+					roleId: parsed.roleId ?? user.roleId
 				}
-			);
+			}
+		);
 
-			throw redirect(303, `${adminPrefix()}/arm`);
-		}
+		throw redirect(303, `${adminPrefix()}/arm`);
 	},
 	resetPassword: async function ({ params }) {
 		const user = await collections.users.findOne({ _id: new ObjectId(params.id) });
