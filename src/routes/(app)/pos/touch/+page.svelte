1c3e1f71f32d55122cd0c8af0eff1ba65835f939
unknown <ithiame@agiltoo.fr>
2024-06-04 14:41:46 +0000

    integrate issue #1298 change

--- src/routes/(app)/pos/touch/+page.svelte
+++ src/routes/(app)/pos/touch/+page.svelte
@@ -9,12 +9,12 @@
 		<div class=" bg-yellow-100"></div>
 		<div class="col-span-2">
 			<div class="grid grid-cols-2 gap-4 text-white text-3xl text-center">
-				<div class="col-span-2 bg-rosebudcherry-500">FAVORIS</div>
-				<div class="bg-rosebudcherry-500">E-pub(salon FR)</div>
-				<div class="bg-rosebudcherry-500">Livre audio CD(salon FR)</div>
-				<div class="bg-rosebudcherry-500">Livre physique(salon FR)</div>
-				<div class="bg-rosebudcherry-500">autres aricles(salon FR)</div>
-				<div class="col-span-2 bg-rosebudcherry-500">TOUS LES ARTICLES</div>
+				<div class="col-span-2 touchScreen-category-cta">FAVORIS</div>
+				<div class="touchScreen-category-cta">E-pub(salon FR)</div>
+				<div class="touchScreen-category-cta">Livre audio CD(salon FR)</div>
+				<div class="touchScreen-category-cta">Livre physique(salon FR)</div>
+				<div class="touchScreen-category-cta">autres aricles(salon FR)</div>
+				<div class="col-span-2 touchScreen-category-cta">TOUS LES ARTICLES</div>
 
 				<div class="col-span-2 bg-gray-400">Liste produits</div>
 			</div>
@@ -22,18 +22,18 @@
 	</div>
 
 	<div class="grid grid-cols-3 gap-4 mt-2">
-		<div class="bg-gray-500 text-3xl p-4 text-center">TICKETS</div>
+		<div class="touchScreen-ticket-menu text-3xl p-4 text-center">TICKETS</div>
 		<div class="col-span-2 grid grid-cols-3 gap-4">
-			<div class="col-span-1 bg-gray-500 text-3xl p-4">SAUVER</div>
-			<div class="col-span-1 bg-gray-500 text-3xl p-4">POOL</div>
-			<div class="col-span-1 bg-gray-500 text-3xl p-4">OUVRIR TIROIR</div>
+			<div class="col-span-1 touchScreen-action-secondaryCTA text-3xl p-4">SAUVER</div>
+			<div class="col-span-1 touchScreen-action-secondaryCTA text-3xl p-4">POOL</div>
+			<div class="col-span-1 touchScreen-action-secondaryCTA text-3xl p-4">OUVRIR TIROIR</div>
 		</div>
 	</div>
 	<div class="grid grid-cols-2 gap-4 mt-2">
-		<div class="bg-gray-500 text-3xl p-4 text-center">PAYER</div>
+		<div class="touchScreen-action-cta text-3xl p-4 text-center">PAYER</div>
 		<div class="grid grid-cols-2 gap-4">
-			<div class="col-span-1 bg-yellow-300 text-3xl p-4 text-center">❎</div>
-			<div class="col-span-1 bg-red-500 text-3xl p-4 text-center">🗑️</div>
+			<div class="col-span-1 touchScreen-action-cancel text-3xl p-4 text-center">❎</div>
+			<div class="col-span-1 touchScreen-action-delete text-3xl p-4 text-center">🗑️</div>
 		</div>
 	</div>
 </main>
