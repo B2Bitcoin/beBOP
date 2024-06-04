7d9dea5db1e6f0b2b305a695b9c80c2ccb12f1ca
unknown <ithiame@agiltoo.fr>
2024-06-04 10:03:51 +0000

    Create pos-touch interface #1298

---
+++ src/routes/(app)/pos/touch/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<meta name="viewport" content="width=1024" />
+</svelte:head>
+<main class="fixed top-0 bottom-0 right-0 left-0 bg-white p-4">
+	<div class="grid grid-cols-3 gap-4">
+		<div class=" bg-yellow-100"></div>
+		<div class="col-span-2">
+			<div class="grid grid-cols-2 gap-4 text-white text-3xl text-center">
+				<div class="col-span-2 bg-rosebudcherry-500">FAVORIS</div>
+				<div class="bg-rosebudcherry-500">E-pub(salon FR)</div>
+				<div class="bg-rosebudcherry-500">Favoris</div>
+				<div class="bg-rosebudcherry-500">Favoris</div>
+				<div class="bg-rosebudcherry-500">Favoris</div>
+				<div class="col-span-2 bg-gray-400">Liste produits</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="grid grid-cols-3 gap-4 mt-2">
+		<div class="bg-gray-500 text-3xl p-4 text-center">TICKETS</div>
+		<div class="col-span-2 grid grid-cols-3 gap-4">
+			<div class="col-span-1 bg-gray-500 text-3xl p-4">SAUVER</div>
+			<div class="col-span-1 bg-gray-500 text-3xl p-4">POOL</div>
+			<div class="col-span-1 bg-gray-500 text-3xl p-4">OUVRIR TIROIR</div>
+		</div>
+	</div>
+	<div class="grid grid-cols-2 gap-4 mt-2">
+		<div class="bg-gray-500 text-3xl p-4 text-center">PAYER</div>
+		<div class="grid grid-cols-2 gap-4">
+			<div class="col-span-1 bg-yellow-300 text-3xl p-4 text-center">❎</div>
+			<div class="col-span-1 bg-red-500 text-3xl p-4 text-center">🗑️</div>
+		</div>
+	</div>
+</main>
