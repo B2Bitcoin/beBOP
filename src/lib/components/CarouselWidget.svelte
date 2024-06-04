dd6146b8c0b03f0b10e66e32ab0b43404db7fad4
ithiame <133223073+ithiame@users.noreply.github.com>
2024-06-04 13:18:12 +0000

    Create a mobile-dedicated specification widget variation #1243 (#1271)

--- src/lib/components/CarouselWidget.svelte
+++ src/lib/components/CarouselWidget.svelte
@@ -1,46 +1,53 @@
 <script lang="ts">
-	import { Carousel } from 'flowbite-svelte';
 	import type { Picture } from '$lib/types/Picture';
+	import { TinySlider } from 'svelte-tiny-slider';
+	import PictureComponent from './Picture.svelte';
+	import { onMount } from 'svelte';
 
 	export let pictures: Picture[];
 	export let autoplay: number;
 	let className = '';
 	export { className as class };
-	let images = pictures.map((picture) => ({
-		alt: picture.name,
-		srcset: picture.storage.formats
-			.map((format) => `/picture/raw/${picture?._id}/format/${format.width} ${format.width}w`)
-			.join(', '),
-		title: picture.name,
-		slide: picture.slider
-	}));
+	let currentIndex = 0;
+	// Function to move to the next slide
+	function nextSlide() {
+		currentIndex = (currentIndex + 1) % pictures.length;
+	}
+	let interval: number;
+	onMount(() => {
+		interval = Number(setInterval(nextSlide, autoplay));
+		return () => clearInterval(interval);
+	});
+	function setIndex(index: number) {
+		currentIndex = index;
+		clearInterval(interval);
+		setInterval(nextSlide, autoplay);
+	}
 </script>
 
 <div class={className}>
-	<Carousel {images} duration={autoplay} transition={null} let:Indicators>
-		<a
-			slot="slide"
-			href={images[index].slide?.url}
-			target={images[index].slide?.openNewTab ? '_blank' : '_self'}
-			let:Slide
-			let:index
-			on:click|preventDefault={() => {
-				// We need to prevent default here because otherwise the link will be opened twice
-				// We have custom handling for mobile devices, because the click event is absorbed by the gesture handler
-				const url = images[index].slide?.url;
-				if (url) {
-					if (images[index].slide?.openNewTab) {
-						window.open(url, '_blank');
-					} else {
-						window.location.href = url;
-					}
-				}
-			}}
-		>
-			<Slide image={images[index]} />
-		</a>
-		{#if images.length > 1}
-			<Indicators />
-		{/if}
-	</Carousel>
+	<TinySlider {currentIndex} transitionDuration={200}>
+		{#each pictures as picture, i}
+			<div class="flex-row">
+				{#if currentIndex === i}
+					<a href={picture.slider?.url} target={picture.slider?.openNewTab ? '_blank' : '_self'}>
+						<PictureComponent {picture} class="object-fill h-auto w-auto " /></a
+					>
+				{/if}
+			</div>
+		{/each}
+		<svelte:fragment slot="controls" let:currentIndex>
+			<!-- Pagination Points -->
+			<div class="relative bottom-10 mx-auto w-full flex justify-center p-4 space-x-2">
+				{#each Array.from({ length: pictures.length }, (_, index) => index) as i}
+					<button
+						class="w-3 h-3 rounded-full transition-colors duration-300"
+						class:body-mainCTA={currentIndex === i}
+						class:bg-gray-200={currentIndex !== i}
+						on:click={() => setIndex(i)}
+					/>
+				{/each}
+			</div>
+		</svelte:fragment>
+	</TinySlider>
 </div>
