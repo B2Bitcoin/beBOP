1fbe7eef3208906126c2a444f6621139ff53bcdd
ithiame <133223073+ithiame@users.noreply.github.com>
2024-06-06 18:08:04 +0000

    Mobile dedicated gallery widget (#1270)
    Co-authored-by: Christophe GERARD <50206014+Tirodem@users.noreply.github.com>

--- src/lib/components/GalleryWidget/GalleryWidget.svelte
+++ src/lib/components/GalleryWidget/GalleryWidget.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { Picture } from '$lib/types/Picture';
-	import type { Gallery } from '$lib/types/Gallery';
+	import type { Gallery, GalleryBase } from '$lib/types/Gallery';
 	import PictureComponent from '../Picture.svelte';
 	import { marked } from 'marked';
+	import { TinySlider } from 'svelte-tiny-slider';
 
 	export let pictures: Picture[];
 	export let gallery: Pick<Gallery, '_id' | 'name' | 'principal' | 'secondary'>;
@@ -11,11 +12,12 @@
 	let className = '';
 	export { className as class };
 	$: pictureById = Object.fromEntries(pictures.map((picture) => [picture._id, picture]));
+	let gallerySecondaryMobile = [...gallery.secondary, gallery.principal];
+	gallerySecondaryMobile.unshift(gallerySecondaryMobile.pop() as GalleryBase);
 </script>
 
-<!-- flex-row-reverse -->
 <div
-	class="mx-auto flex rounded justify-center {className} {displayOption === 'var-2'
+	class="mx-auto hidden lg:flex rounded justify-center {className} {displayOption === 'var-2'
 		? 'flex-row-reverse'
 		: ''}"
 >
@@ -68,4 +70,65 @@
 			</div>
 		</div>
 	{/each}
+</div>
+
+<div
+	class="relative mx-auto lg:hidden flex rounded justify-center {className} {displayOption ===
+	'var-2'
+		? 'flex-row-reverse'
+		: ''}"
+>
+	<TinySlider let:currentIndex>
+		{#each Array.from({ length: gallerySecondaryMobile.length }, (_, index) => index) as i}
+			<div class="grid grid-cols-6 justify-center">
+				{#if currentIndex === i}
+					<div class="col-span-6 p-2 m-2 mx-12 tagWidget tagWidget-main text-center">
+						<h2 class="text-xl body-title pb-2 uppercase">
+							{gallerySecondaryMobile[i].title}
+						</h2>
+					</div>
+					<div class="col-span-6 m-2 justify-center items-center">
+						<PictureComponent
+							picture={pictureById[gallerySecondaryMobile[i].pictureId || '']}
+							class="block h-auto w-auto"
+						/>
+					</div>
+					<div class="col-span-6 m-2 tagWidget tagWidget-main text-center">
+						<p class="m-2">
+							<!-- eslint-disable svelte/no-at-html-tags -->
+							{@html marked(gallerySecondaryMobile[i].content.replaceAll('<', '&lt;'))}
+						</p>
+					</div>
+					<div class="col-span-6 btn tagWidget-cta text-xl w-auto p-4 m-2">
+						<a
+							class="tagWidget-hyperlink"
+							href={gallerySecondaryMobile[i].cta.href}
+							target={gallerySecondaryMobile[i].cta.href.startsWith('http') ||
+							gallerySecondaryMobile[i].cta.openNewTab
+								? '_blank'
+								: '_self'}>{gallerySecondaryMobile[i].cta.label}</a
+						>
+					</div>
+				{/if}
+			</div>
+		{/each}
+		<svelte:fragment slot="controls" let:setIndex let:currentIndex>
+			<div class="absolute top-0 left-0 flex items-center py-3">
+				{#if currentIndex > 0}
+					<button
+						class="body-mainCTA px-4 py-2 rounded-full"
+						on:click={() => setIndex(currentIndex - 1)}>&#9664;</button
+					>
+				{/if}
+			</div>
+			<div class="absolute top-0 right-0 flex items-center py-3">
+				{#if currentIndex < gallerySecondaryMobile.length - 1}
+					<button
+						class="body-mainCTA px-4 py-2 rounded-full"
+						on:click={() => setIndex(currentIndex + 1)}>&#9654;</button
+					>
+				{/if}
+			</div>
+		</svelte:fragment>
+	</TinySlider>
 </div>
