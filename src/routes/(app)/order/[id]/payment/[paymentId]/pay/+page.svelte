971aca9cc75d705da996125feafa08b2c92ab79c
coyotte508 <coyotte508@gmail.com>
2024-07-18 17:50:22 +0200

    ✨ Stripe payment widget

--- src/routes/(app)/order/[id]/payment/[paymentId]/pay/+page.svelte
+++ src/routes/(app)/order/[id]/payment/[paymentId]/pay/+page.svelte
@@ -114,6 +114,53 @@
 					merchantName: string;
 				};
 			};
+
+			Stripe(publicKey: string): {
+				elements: (options: {
+					locale?: string;
+					appearance: {
+						theme: 'stripe' | 'night' | 'flat';
+						variables?: {
+							colorPrimary?: string;
+							colorBackground?: string;
+							colorText?: string;
+							colorDanger?: string;
+							fontFamily?: string;
+							spacingUnit?: string;
+							borderRadius?: string;
+							fontSizeBase?: string;
+							// See more here: https://docs.stripe.com/elements/appearance-api?platform=web#additional-variables
+						};
+					};
+					clientSecret: string;
+				}) => {
+					create: (
+						type: 'card' | 'cardNumber' | 'cardExpiry' | 'cardCvc' | 'payment',
+						options: {
+							// https://docs.stripe.com/js/elements_object/create_payment_element#payment_element_create-options-layout-defaultCollapsed
+							layout:
+								| {
+										type: 'accordion' | 'tabs';
+										defaultCollapsed?: boolean;
+
+										// Only for accordion
+										radios?: boolean;
+										spacedAccordionItems?: boolean;
+										visibleAccordionItemsCount?: number;
+								  }
+								| 'accordion'
+								| 'tabs';
+						}
+					) => any;
+				};
+				createToken: (element: any) => Promise<{ token: string }>;
+				confirmPayment: (options: {
+					elements: any;
+					confirmParams: {
+						return_url: string;
+					};
+				}) => Promise<{ error: { type: string; message: string } }>;
+			};
 		}
 	}
 </script>
@@ -128,33 +175,101 @@
 
 	export let data;
 
+	let paymentLoading = false;
+
+	$: orderPath = '/order/' + $page.params.id;
+
 	function mountSumUpCard() {
 		// Should always be true due to backend validation, doing this for TS
 		if (data.payment.checkoutId) {
 			window.SumUpCard.mount({
-				id: 'sumup-card',
+				id: 'payment-element',
 				checkoutId: data.payment.checkoutId,
 				onResponse: function (type) {
 					if (type === 'success') {
-						goto('/order/' + $page.params.id);
+						goto(orderPath);
 					}
 				}
 			});
 		}
 	}
 
-	onMount(() => {
-		if (window.SumUpCard) {
-			mountSumUpCard();
-		} else {
-			document.head.append(
-				Object.assign(document.createElement('script'), {
-					src: 'https://gateway.sumup.com/gateway/ecom/card/v2/sdk.js',
-					async: false,
-					defer: false,
-					onload: () => mountSumUpCard()
+	function mountStripeCard() {
+		// Actually more like "clientSecret"
+		if (data.payment.checkoutId && data.stripePublicKey) {
+			const stripe = window.Stripe(data.stripePublicKey);
+			const elements = stripe.elements({
+				appearance: {
+					theme: 'stripe'
+				},
+				clientSecret: data.payment.checkoutId
+			});
+
+			elements
+				.create('payment', {
+					layout: 'tabs'
 				})
-			);
+				.mount('#payment-element');
+
+			handleSubmit = async () => {
+				try {
+					paymentLoading = true;
+
+					const { error } = await stripe.confirmPayment({
+						elements,
+						confirmParams: {
+							// Make sure to change this to your payment completion page
+							return_url: window.location.origin + orderPath
+						}
+					});
+
+					// This point will only be reached if there is an immediate error when
+					// confirming the payment. Otherwise, your customer will be redirected to
+					// your `return_url`. For some payment methods like iDEAL, your customer will
+					// be redirected to an intermediate site first to authorize the payment, then
+					// redirected to the `return_url`.
+					if (error.type === 'card_error' || error.type === 'validation_error') {
+						alert(error.message);
+					} else {
+						alert('An unexpected error occurred.');
+					}
+				} finally {
+					paymentLoading = false;
+				}
+			};
+		}
+	}
+
+	onMount(() => {
+		if (data.payment.processor === 'sumup') {
+			if (window.SumUpCard) {
+				mountSumUpCard();
+			} else {
+				document.head.append(
+					Object.assign(document.createElement('script'), {
+						src: 'https://gateway.sumup.com/gateway/ecom/card/v2/sdk.js',
+						async: false,
+						defer: false,
+						onload: () => mountSumUpCard()
+					})
+				);
+			}
+		} else if (data.payment.processor === 'stripe') {
+			// Stripe
+			if ('Stripe' in window) {
+				mountStripeCard();
+			} else {
+				document.head.append(
+					Object.assign(document.createElement('script'), {
+						src: 'https://js.stripe.com/v3/',
+						async: false,
+						defer: false,
+						onload: () => {
+							mountStripeCard();
+						}
+					})
+				);
+			}
 		}
 	});
 
@@ -166,11 +281,23 @@
 			document.head.removeChild(script);
 		}
 	});
+
+	let handleSubmit = () => {};
 </script>
 
 <main class="mx-auto max-w-7xl py-10 px-6 flex flex-col md:flex-row gap-4 justify-around">
 	<div class="grow">
-		<div id="sumup-card" class="max-w-lg mx-auto bg-white text-gray-700 rounded shadow"></div>
+		{#if data.payment.processor === 'stripe'}
+			<form class="payment-form" on:submit|preventDefault={handleSubmit}>
+				<div id="payment-element" class="stripe"></div>
+				<button class="btn btn-black" type="submit" disabled={paymentLoading}>Pay now</button>
+			</form>
+		{:else}
+			<div
+				id="payment-element"
+				class="max-w-lg mx-auto bg-white text-gray-700 rounded shadow {data.payment.processor}"
+			></div>
+		{/if}
 	</div>
 	<div class="self-center md:self-stretch">
 		<OrderSummary order={data.order} class="sticky top-4 -mt-1" />
@@ -178,7 +305,7 @@
 </main>
 <!-- 
 <style>
-	#sumup-card {
+	#payment-element.sumup {
 		--cui-border-danger: #f44336;
 		--cui-border-normal: #ccc;
 		--cui-border-focus: #ebf4ff;
