3325eb0582a55c505cee7eeec27d472b5a319675
Eliott C. <coyotte508@gmail.com>
2024-09-03 05:34:44 +0200

    🌐 Translate to dutch (#1410)
    Co-authored-by: Christophe GERARD <50206014+Tirodem@users.noreply.github.com>

--- src/lib/translations/index.ts
+++ src/lib/translations/index.ts
@@ -2,12 +2,14 @@ import { merge } from 'lodash-es';
 import en from './en.json';
 import es_sv from './es-sv.json';
 import fr from './fr.json';
+import nl from './nl.json';
 import { typedKeys } from '$lib/utils/typedKeys';
 
 export const languages = {
 	en,
 	'es-sv': es_sv,
-	fr
+	fr,
+	nl
 };
 
 export const enhancedLanguages = merge({}, languages);
@@ -19,5 +21,6 @@ export type LanguageKey = keyof typeof languages;
 export const languageNames: Record<LanguageKey, string> = {
 	en: 'English',
 	'es-sv': 'Español (El Salvador)',
-	fr: 'Français'
+	fr: 'Français',
+	nl: 'Nederlands'
 };
