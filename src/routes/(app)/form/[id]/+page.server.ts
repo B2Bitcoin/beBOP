21f144574b1c2e01ac54155cef2691c3369d7128
unknown <ithiame@agiltoo.fr>
2024-09-03 09:35:55 +0000

    fix suggestion: remove target input on form

--- src/routes/(app)/form/[id]/+page.server.ts
+++ src/routes/(app)/form/[id]/+page.server.ts
@@ -31,14 +31,14 @@ export const load = async ({ params, locals }) => {
 };
 
 export const actions = {
-	sendEmail: async function ({ request, locals }) {
+	sendEmail: async function ({ request, locals, params }) {
+		const contactForm = await collections.contactForms.findOne({ _id: params.id });
 		rateLimit(locals.clientIp, 'email', 5, { minutes: 5 });
 
 		const data = await request.formData();
 		const parsed = z
 			.object({
 				content: z.string().max(MAX_CONTENT_LIMIT),
-				target: z.string().max(100),
 				subject: z.string().max(100),
 				from: z.string().max(100).optional()
 			})
@@ -51,26 +51,27 @@ export const actions = {
 		const content = `Message envoyé par formulaire sur le site ${ORIGIN} Adresse de contact : ${
 			parsed.from ? parsed.from : 'non-renseigné'
 		}   Message envoyé : ${parsed.content}`;
-		if (parsed.target.startsWith('npub')) {
-			await collections.nostrNotifications.insertOne({
-				_id: new ObjectId(),
-				createdAt: new Date(),
-				kind: Kind.EncryptedDirectMessage,
-				updatedAt: new Date(),
-				content,
-				dest: parsed.target
-			});
-		} else {
-			await collections.emailNotifications.insertOne({
-				_id: new ObjectId(),
-				createdAt: new Date(),
-				updatedAt: new Date(),
-				subject: parsed.subject,
-				htmlContent: htmlContent,
-				dest: parsed.target || SMTP_USER
-			});
+		if (contactForm) {
+			if (contactForm.target.startsWith('npub')) {
+				await collections.nostrNotifications.insertOne({
+					_id: new ObjectId(),
+					createdAt: new Date(),
+					kind: Kind.EncryptedDirectMessage,
+					updatedAt: new Date(),
+					content,
+					dest: contactForm.target
+				});
+			} else {
+				await collections.emailNotifications.insertOne({
+					_id: new ObjectId(),
+					createdAt: new Date(),
+					updatedAt: new Date(),
+					subject: parsed.subject,
+					htmlContent: htmlContent,
+					dest: contactForm.target || SMTP_USER
+				});
+			}
 		}
-
 		throw redirect(303, request.headers.get('referer') || '/');
 	}
 };
