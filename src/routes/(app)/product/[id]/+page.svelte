70c68169a8a9473c872d0b113efcb874affade1b
unknown <ithiame@agiltoo.fr>
2024-10-02 10:43:38 +0000

    Add selection item to product (with no specific price nor specific stock) #1466

--- src/routes/(app)/product/[id]/+page.svelte
+++ src/routes/(app)/product/[id]/+page.svelte
@@ -20,7 +20,6 @@
 	import { useI18n } from '$lib/i18n';
 	import CmsDesign from '$lib/components/CmsDesign.svelte';
 	import { FRACTION_DIGITS_PER_CURRENCY, CURRENCY_UNIT } from '$lib/types/Currency.js';
-	import { each } from 'lodash-es';
 
 	export let data;
 
