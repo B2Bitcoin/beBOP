5647e218913574de6bb4cbb264db04919fe4c626
unknown <ithiame@agiltoo.fr>
2024-10-08 15:57:03 +0000

    fix error

--- src/lib/components/CmsDesign.svelte
+++ src/lib/components/CmsDesign.svelte
@@ -173,14 +173,10 @@
 			{:else if token.type === 'pictureWidget'}
 				<PictureComponent
 					picture={pictureById[token.slug]}
-					class="my-5 {token.height ? `lg:h-[${token.height}px]` : ''} {token.width
-						? `lg:w-[${token.width}px]`
-						: ''}"
-					style={`@media (min-width: 1024px) {
-						${token.height ? `height: ${token.height}px;` : ''}
-						${token.width ? `width: ${token.width}px;` : ''}
-						${token.fit ? `object-fit: ${token.fit};` : ''}
-					  }`}
+					class="my-5 
+         			{token.height ? `lg:h-[${token.height}px]` : ''} 
+         			{token.width ? `lg:w-[${token.width}px]` : ''} 
+         			{token.fit ? `lg:object-${token.fit}` : ''}"
 				/>
 			{:else if token.type === 'html'}
 				<div class="my-5">
@@ -257,14 +253,10 @@
 				{:else if token.type === 'pictureWidget'}
 					<PictureComponent
 						picture={pictureById[token.slug]}
-						class="my-5 {token.height ? `lg:h-[${token.height}px]` : ''} {token.width
-							? `lg:w-[${token.width}px]`
-							: ''}"
-						style="{token.fit
-							? `@media (min-width: 1024px) {object-fit: ${token.fit};}`
-							: ''}{token.width
-							? `@media (min-width: 1024px) {width: ${token.width}px;}`
-							: ''}{token.height ? `@media (min-width: 1024px) {height: ${token.height}px;}` : ''}"
+						class="my-5 
+         			{token.height ? `lg:h-[${token.height}px]` : ''} 
+         			{token.width ? `lg:w-[${token.width}px]` : ''} 
+         			{token.fit ? `lg:object-${token.fit}` : ''}"
 					/>
 				{:else if token.type === 'html'}
 					<div class="my-5">
