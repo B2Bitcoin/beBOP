c8d8e7e2a748d57a3db6c45ed9ac9f30d9649bc0
unknown <ithiame@agiltoo.fr>
2024-10-10 12:20:19 +0000

    fix suggestion

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,7 +1,18 @@
 import { runtimeConfig } from '$lib/server/runtime-config';
 
 export function GET() {
-	if (!runtimeConfig.hideFromSearchEngines) {
+	if (runtimeConfig.hideFromSearchEngines) {
+		return new Response(
+			`User-agent: *
+Disallow: /
+`,
+			{
+				headers: {
+					'content-type': 'text/plain'
+				}
+			}
+		);
+	} else {
 		return new Response(
 			`User-agent: *
 Disallow: /order/
@@ -14,17 +25,6 @@ Disallow: /login/
 Disallow: /orders/
 Disallow: /identity/
 Disallow: /pos/
-`,
-			{
-				headers: {
-					'content-type': 'text/plain'
-				}
-			}
-		);
-	} else {
-		return new Response(
-			`User-agent: *
-Disallow: /
 `,
 			{
 				headers: {
