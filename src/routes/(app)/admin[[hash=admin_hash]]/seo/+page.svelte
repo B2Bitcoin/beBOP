d8d51e91594cc3795b8b837d44e1359b12dd551e
ithiame <133223073+ithiame@users.noreply.github.com>
2024-10-10 22:29:47 +0000

    Seo cloack (#1494)

---
+++ src/routes/(app)/admin[[hash=admin_hash]]/seo/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let data;
+	export let form;
+</script>
+
+<h1 class="text-2xl">SEO Config</h1>
+{#if form?.success}
+	<div class="alert alert-success">{form.success}</div>
+{/if}
+<form method="post" class="flex flex-col gap-4" action="?/update">
+	<label class="checkbox-label">
+		<input
+			type="checkbox"
+			name="hideFromSearchEngines"
+			class="form-checkbox"
+			checked={data.hideFromSearchEngines}
+		/>
+		Hide from search engines
+	</label>
+	<input type="submit" value="Update" class="btn btn-gray self-start" />
+</form>
--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,17 +1,36 @@
+import { runtimeConfig } from '$lib/server/runtime-config';
+
 export function GET() {
-	return new Response(
-		`User-agent: *
+	if (runtimeConfig.hideFromSearchEngines) {
+		return new Response(
+			`User-agent: *
+Disallow: /
+`,
+			{
+				headers: {
+					'content-type': 'text/plain'
+				}
+			}
+		);
+	} else {
+		return new Response(
+			`User-agent: *
 Disallow: /order/
 Disallow: /subscription/
 Disallow: /admin/
 Disallow: /admin-*/
 Disallow: /checkout
 Disallow: /cart
+Disallow: /login/
+Disallow: /orders/
+Disallow: /identity/
+Disallow: /pos/
 `,
-		{
-			headers: {
-				'content-type': 'text/plain'
+			{
+				headers: {
+					'content-type': 'text/plain'
+				}
 			}
-		}
-	);
+		);
+	}
 }
