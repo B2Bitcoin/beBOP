af044702e368a3d6a6d0e3b566d118c9340cff00
unknown <ithiame@agiltoo.fr>
2024-10-10 09:49:35 +0000

    seo save runtimeConfig enableSEO

--- src/routes/(app)/admin[[hash=admin_hash]]/seo/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/seo/+page.server.ts
@@ -1,3 +1,32 @@
+import { collections } from '$lib/server/database';
+import { runtimeConfig } from '$lib/server/runtime-config.js';
+import { z } from 'zod';
+
 export async function load() {}
 
-export const actions = {};
+export const actions = {
+	update: async function ({ request }) {
+		const formData = await request.formData();
+
+		const result = z
+			.object({
+				enableSEO: z.boolean({ coerce: true })
+			})
+			.parse({
+				enableSEO: formData.get('enableSEO')
+			});
+
+		await collections.runtimeConfig.updateOne(
+			{ _id: 'enableSEO' },
+			{
+				$set: { data: result.enableSEO, updatedAt: new Date() },
+				$setOnInsert: { createdAt: new Date() }
+			},
+			{ upsert: true }
+		);
+		runtimeConfig.enableSEO = result.enableSEO;
+		return {
+			success: 'SEO configuration updated.'
+		};
+	}
+};
--- src/routes/(app)/admin[[hash=admin_hash]]/seo/+page.svelte
+++ src/routes/(app)/admin[[hash=admin_hash]]/seo/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	export let data;
+	export let form;
 </script>
 
 <h1 class="text-2xl">SEO Config</h1>
-<form method="post" class="flex flex-col gap-4">
+{#if form?.success}
+	<div class="alert alert-success">{form.success}</div>
+{/if}
+<form method="post" class="flex flex-col gap-4" action="?/update">
 	<label class="checkbox-label">
 		<input type="checkbox" name="enableSEO" class="form-checkbox" checked={data.enableSEO} />
 		Enable SEO cloak
 	</label>
+	<input type="submit" value="Update" class="btn btn-gray self-start" />
 </form>
