218c6a01a87a1d5330a40bd511237402d8acb22d
unknown <ithiame@agiltoo.fr>
2024-10-15 10:24:58 +0000

    reporting filter

--- src/routes/(app)/admin[[hash=admin_hash]]/reporting/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/reporting/+page.server.ts
@@ -4,12 +4,18 @@ import { sum } from '$lib/utils/sum';
 import { pojo } from '$lib/server/pojo.js';
 import { endOfMonth, startOfMonth } from 'date-fns';
 
-export async function load() {
+export async function load({ url }) {
+	const month = Number(url.searchParams.get('month'));
+	const year = Number(url.searchParams.get('year'));
+	let date = new Date();
+	if (year && month) {
+		date = new Date(year, month - 1);
+	}
 	const orders = await collections.orders
 		.find({
 			createdAt: {
-				$gte: startOfMonth(new Date()),
-				$lt: endOfMonth(new Date())
+				$gte: startOfMonth(date),
+				$lt: endOfMonth(date)
 			}
 		})
 		.sort({ createdAt: -1 })
