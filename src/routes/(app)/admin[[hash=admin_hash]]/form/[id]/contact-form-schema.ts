45739ad3afc699640e8ebffeb17b15b17fc1cf54
unknown <ithiame@agiltoo.fr>
2024-10-25 14:51:26 +0000

    🐛 A form cannot be created without content #1527

--- src/routes/(app)/admin[[hash=admin_hash]]/form/[id]/contact-form-schema.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/form/[id]/contact-form-schema.ts
@@ -2,7 +2,7 @@ import { MAX_CONTENT_LIMIT } from '$lib/types/CmsPage';
 import { z } from 'zod';
 
 export const contactFormTranslatableSchema = {
-	content: z.string().max(MAX_CONTENT_LIMIT),
+	content: z.string().max(MAX_CONTENT_LIMIT).optional(),
 	subject: z.string().max(100),
 	disclaimer: z
 		.object({
