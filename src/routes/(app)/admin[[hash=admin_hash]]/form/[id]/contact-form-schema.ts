35c48e634852af3e62bb1239017c1a1dd19bf165
ithiame <133223073+ithiame@users.noreply.github.com>
2024-10-29 11:09:59 +0000

    🐛 A form cannot be created without content #1527 (#1545)

--- src/routes/(app)/admin[[hash=admin_hash]]/form/[id]/contact-form-schema.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/form/[id]/contact-form-schema.ts
@@ -2,12 +2,12 @@ import { MAX_CONTENT_LIMIT } from '$lib/types/CmsPage';
 import { z } from 'zod';
 
 export const contactFormTranslatableSchema = {
-	content: z.string().max(MAX_CONTENT_LIMIT),
+	content: z.string().max(MAX_CONTENT_LIMIT).default(''),
 	subject: z.string().max(100),
 	disclaimer: z
 		.object({
 			label: z.string().min(1).max(100).trim(),
-			content: z.string().min(1).max(MAX_CONTENT_LIMIT).trim(),
+			content: z.string().max(MAX_CONTENT_LIMIT).trim(),
 			checkboxLabel: z.string().min(1).max(100).trim()
 		})
 		.optional()
