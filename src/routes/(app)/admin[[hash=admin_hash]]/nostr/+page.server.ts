a93522ffd18799b61b11ff7cec76d28590235d6c
ithiame <ithiame@agiltoo.fr>
2024-11-19 08:38:36 +0000

    fix suggested changes7

--- src/routes/(app)/admin[[hash=admin_hash]]/nostr/+page.server.ts
+++ src/routes/(app)/admin[[hash=admin_hash]]/nostr/+page.server.ts
@@ -17,7 +17,7 @@ import { setTimeout } from 'node:timers/promises';
 import type { Event } from 'nostr-tools';
 import { uniqBy } from '$lib/utils/uniqBy';
 import { NOSTR_PROTOCOL_VERSION } from '$lib/server/locks/handle-messages';
-import { phoenixdLndAddress } from '$lib/server/phoenixd';
+import { isPhoenixdConfigured, phoenixdLndAddress } from '$lib/server/phoenixd';
 
 export function load() {
 	return {
@@ -48,7 +48,9 @@ export const actions = {
 			: null;
 
 		const lndInfo = isLightningConfigured ? await lndGetInfo() : null;
-		const lnAddress = lndInfo?.uris?.[0] ?? (await phoenixdLndAddress());
+		const lnAddress =
+			lndInfo?.uris?.[0] ??
+			(isPhoenixdConfigured() ? await phoenixdLndAddress().catch(() => null) : null);
 
 		await collections.nostrNotifications.insertOne({
 			_id: new ObjectId(),
