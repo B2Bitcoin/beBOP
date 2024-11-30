e1e1489759d805d57d2b788c510a4a8bd7cc2d08
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2024-11-30 17:18:00 +0100

    Manage nostr bot certify (#1603)
    Co-authored-by: ithiame <ithiame@agiltoo.fr>

--- src/lib/server/phoenixd.ts
+++ src/lib/server/phoenixd.ts
@@ -34,6 +34,21 @@ export async function phoenixdBalance(): Promise<{ balanceSat: number; feeCredit
 	return await res.json();
 }
 
+export async function phoenixdLndAddress(): Promise<string> {
+	const res = await fetch(`${runtimeConfig.phoenixd.url}/getlnaddress`, {
+		headers: {
+			Authorization: `Basic ${Buffer.from(`:${runtimeConfig.phoenixd.password}`).toString(
+				'base64'
+			)}`
+		}
+	});
+	if (!res.ok) {
+		throw error(500, `Could not get lnaddress: ${res.status} ${await res.text()}`);
+	}
+
+	return await res.text();
+}
+
 export async function phoenixdDetected(url?: string): Promise<boolean> {
 	return await Promise.race<boolean>([
 		fetch(`${url || runtimeConfig.phoenixd.url}/getinfo`).then(
