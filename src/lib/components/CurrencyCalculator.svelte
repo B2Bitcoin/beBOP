1458b9d355b653fef671b8dec5549b3610c128e2
ithiame <ithiame@agiltoo.fr>
2024-12-05 08:21:15 +0000

    🦋 Currency calculator CMS #1612

--- src/lib/components/CurrencyCalculator.svelte
+++ src/lib/components/CurrencyCalculator.svelte
@@ -3,10 +3,10 @@
 	import { toCurrency } from '$lib/utils/toCurrency';
 	import type { Currency } from '$lib/types/Currency';
 
-	$: firstDeviseValue = 1;
-	$: firstDevise = 'BTC' as Currency;
-	$: secondDevise = 'EUR' as Currency;
-	$: secondDeviseValue = toCurrency(secondDevise, firstDeviseValue, firstDevise);
+	$: firstCurrencyAmount = 1;
+	$: firstCurrency = 'BTC' as Currency;
+	$: secondCurrency = 'EUR' as Currency;
+	$: secondCurrencyAmount = toCurrency(secondCurrency, firstCurrencyAmount, firstCurrency);
 </script>
 
 <div class="flex justify-center mx-auto max-w-3xl bg-gray-800 p-4 rounded-md">
@@ -16,15 +16,15 @@
 		<input
 			type="number"
 			class="form-input p-2 bg-transparent text-left text-white focus:outline-none"
-			bind:value={firstDeviseValue}
+			bind:value={firstCurrencyAmount}
 			min="0"
 		/>
 		<select
 			class="p-2 bg-gray-700 text-white border-l border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-			bind:value={firstDevise}
+			bind:value={firstCurrency}
 		>
 			{#each CURRENCIES as currency}
-				<option value={currency} selected={currency === firstDevise}>{currency}</option>
+				<option value={currency} selected={currency === firstCurrency}>{currency}</option>
 			{/each}
 		</select>
 	</div>
@@ -37,15 +37,15 @@
 		<input
 			type="text"
 			class="form-input p-2 bg-transparent text-left text-white focus:outline-none"
-			bind:value={secondDeviseValue}
+			bind:value={secondCurrencyAmount}
 			readonly
 		/>
 		<select
 			class="p-2 bg-gray-700 text-white border-l border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-			bind:value={secondDevise}
+			bind:value={secondCurrency}
 		>
 			{#each CURRENCIES as currency}
-				<option value={currency} selected={currency === secondDevise}>{currency}</option>
+				<option value={currency} selected={currency === secondCurrency}>{currency}</option>
 			{/each}
 		</select>
 	</div>
