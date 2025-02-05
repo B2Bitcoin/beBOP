0f2daaba655a358f33431ffdea6cac839aa1ced9
coyotte508 <coyotte508@protonmail.com>
2025-02-05 01:04:30 +0100

    remove 2 extra vars

--- src/routes/(app)/admin[[hash=admin_hash]]/reporting/+page.svelte
+++ src/routes/(app)/admin[[hash=admin_hash]]/reporting/+page.svelte
@@ -27,8 +27,6 @@
 			(includeCanceled && order.status === 'canceled') ||
 			(includePartiallyPaid && order.payments.find((payment) => payment.status === 'paid'))
 	);
-	$: quantityOfPaymentMeanMonthYear = quantityOfPaymentMean(paidOrders);
-	$: quantityOfProductMonthYear = quantityOfProduct(paidOrders);
 	$: orderSynthesis = {
 		orderQuantity: sum(paidOrders.map((order) => order.quantityOrder)),
 		orderNumber: paidOrders.length,
@@ -473,7 +471,7 @@
 				</thead>
 				<tbody>
 					<!-- Order rows -->
-					{#each Object.entries(quantityOfProductMonthYear).sort((a, b) => b[1].quantity - a[1].quantity) as [productId, { quantity, total }]}
+					{#each Object.entries(quantityOfProduct(paidOrders)).sort((a, b) => b[1].quantity - a[1].quantity) as [productId, { quantity, total }]}
 						<tr class="hover:bg-gray-100 whitespace-nowrap">
 							<td class="border border-gray-300 px-4 py-2"
 								><time datetime={data.beginsAt.toISOString()}>
@@ -520,7 +518,7 @@
 				</thead>
 				<tbody>
 					<!-- Order rows -->
-					{#each Object.entries(quantityOfPaymentMeanMonthYear).sort((a, b) => b[1].quantity - a[1].quantity) as [method, { quantity, total }]}
+					{#each Object.entries(quantityOfPaymentMean(paidOrders)).sort((a, b) => b[1].quantity - a[1].quantity) as [method, { quantity, total }]}
 						<tr class="hover:bg-gray-100 whitespace-nowrap">
 							<td class="border border-gray-300 px-4 py-2"
 								><time datetime={data.beginsAt.toISOString()}>
