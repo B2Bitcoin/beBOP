784f92783ba85ceb4c6d1b9e516003bbea7fdb61
Christophe GERARD <50206014+Tirodem@users.noreply.github.com>
2025-03-10 17:56:23 +0100

    Update +page.svelte

--- src/routes/(app)/checkout/+page.svelte
+++ src/routes/(app)/checkout/+page.svelte
@@ -569,7 +569,6 @@
 											(!emails[key] || !data.contactModes.includes('email')) &&
 											data.roleId !== POS_ROLE_ID}
 										on:change={(ev) => ev.currentTarget.setCustomValidity('')}
-										autocomplete="off"
 									/>
 								</label>
 							{/if}
